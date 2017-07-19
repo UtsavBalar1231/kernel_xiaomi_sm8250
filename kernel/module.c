979fb8af32bc403cafa54bd83c1e57e10d2969a3
Christopher N. Hesse <raymanfx@gmail.com>
2017-07-20 01:58:26 +0200

    kernel: module: Ignore all magic mismatches
    Change-Id: I3b9012574aa0069131cfd32aecdefa6b00b907bb
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/module.c
+++ kernel/module.c
@@ -3686,6 +3686,10 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	long err = 0;
 	char *after_dashes;
 
+	//FIXME
+	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
+	flags |= MODULE_INIT_IGNORE_VERMAGIC;
+
 	err = elf_header_check(info);
 	if (err)
 		goto free_copy;
