5d2d10b456522fe034d369fbfab6e41a6cfd5807
Adrian Salido <salidoa@google.com>
2017-12-04 16:52:17 -0800

    techpack: display: drm/msm: add idle state sysfs node
    Add a sysfs mechanism to track the idle state of display subsystem.
    This allows user space to poll on the idle state node to detect when
    display goes idle for longer than the time set.
    
    Bug: 139655049
    Bug: 126304228
    Change-Id: I21e3c7b0830a9695db9f65526c111ce5153d1764
    Signed-off-by: Adrian Salido <salidoa@google.com>
    Signed-off-by: Robb Glasser <rglasser@google.com>
    (cherry picked from commit 11a2193b434cb3130743fbff89a161062883132e)
    Signed-off-by: Ken Huang <kenbshuang@google.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- techpack/display/msm/msm_drv.h
+++ techpack/display/msm/msm_drv.h
@@ -596,6 +596,15 @@ struct msm_drm_thread {
 	struct kthread_worker worker;
 };
 
+struct msm_idle {
+	u32 timeout_ms;
+	u32 encoder_mask;
+	u32 active_mask;
+
+	spinlock_t lock;
+	struct delayed_work work;
+};
+
 struct msm_drm_private {
 
 	struct drm_device *dev;
@@ -707,6 +716,8 @@ struct msm_drm_private {
 	/* update the flag when msm driver receives shutdown notification */
 	bool shutdown_in_progress;
 	ktime_t  complete_commit_time;
+
+	struct msm_idle idle;
 };
 
 /* get struct msm_kms * from drm_device * */
@@ -973,6 +984,7 @@ static inline void __exit msm_mdp_unregister(void)
 }
 #endif
 
+void msm_idle_set_state(struct drm_encoder *encoder, bool active);
 #ifdef CONFIG_DEBUG_FS
 void msm_gem_describe(struct drm_gem_object *obj, struct seq_file *m);
 void msm_gem_describe_objects(struct list_head *list, struct seq_file *m);
