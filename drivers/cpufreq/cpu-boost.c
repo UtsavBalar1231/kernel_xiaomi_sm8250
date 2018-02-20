72f6148a7e6005389a14a0f8c6dc2e6665ca56b2
Alex Naidis <alex.naidis@paranoidandroid.co>
2018-02-20 18:02:13 -0500

    cpu-boost: Bind to LITTLE cpus and reduce RT prio
    To prevent the case of a possible RT priority inversion with
    more important RT tasks like mdss (RIP), reduce the priority to 2.
    This will make sure cpu-boost can take priority over most work
    items while not causing a priority inversion leading to thread
    interruption.
    
    Additionally we bind the cpu-boost worker thread to CPUs 0-3 to
    decrease the likeliness of it interfering with the app threads.
    For that purpose, we do a small refactor of the kthread init process.
    
    While at it, add logging for the case where sched_setscheduler fails,
    as it can fail due to security checks.
    
    Change-Id: Ie33da4414e8427f10da2bfdbaa3f119120afffa5
    Signed-off-by: Alex Naidis <alex.naidis@paranoidandroid.co>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -336,17 +336,35 @@ static struct input_handler cpuboost_input_handler = {
 struct kobject *cpu_boost_kobj;
 static int cpu_boost_init(void)
 {
-	int cpu, ret;
+	int cpu, ret, i;
 	struct cpu_sync *s;
-	struct sched_param param = { .sched_priority = MAX_RT_PRIO - 2 };
+	struct sched_param param = { .sched_priority = 2 };
+	cpumask_t sys_bg_mask;
+
+	/* Hardcode the cpumask to bind the kthread to it */
+	cpumask_clear(&sys_bg_mask);
+	for (i = 0; i <= 3; i++) {
+		cpumask_set_cpu(i, &sys_bg_mask);
+	}
 
 	kthread_init_worker(&cpu_boost_worker);
-	cpu_boost_worker_thread = kthread_run(kthread_worker_fn,
+	cpu_boost_worker_thread = kthread_create(kthread_worker_fn,
 		&cpu_boost_worker, "cpu_boost_worker_thread");
-	if (IS_ERR(cpu_boost_worker_thread))
+	if (IS_ERR(cpu_boost_worker_thread)) {
+		pr_err("cpu-boost: Failed to init kworker!\n");
 		return -EFAULT;
+	}
 
-	sched_setscheduler(cpu_boost_worker_thread, SCHED_FIFO, &param);
+	ret = sched_setscheduler(cpu_boost_worker_thread, SCHED_FIFO, &param);
+	if (ret)
+		pr_err("cpu-boost: Failed to set SCHED_FIFO!\n");
+
+	/* Now bind it to the cpumask */
+	kthread_bind_mask(cpu_boost_worker_thread, &sys_bg_mask);
+
+	/* Wake it up! */
+	wake_up_process(cpu_boost_worker_thread);
+
 	kthread_init_work(&input_boost_work, do_input_boost);
 	INIT_DELAYED_WORK(&input_boost_rem, do_input_boost_rem);
 
