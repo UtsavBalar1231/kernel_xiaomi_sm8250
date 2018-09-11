a505ccd58fd3c55d7485e155b81cc2763d2c6e96
Kirill Tkhai <ktkhai@virtuozzo.com>
2018-09-11 13:12:14 +0300

    BACKPORT: fuse: Use hash table to link processing request
    We noticed the performance bottleneck in FUSE running our Virtuozzo storage
    over rdma. On some types of workload we observe 20% of times spent in
    request_find() in profiler.  This function is iterating over long requests
    list, and it scales bad.
    
    The patch introduces hash table to reduce the number of iterations, we do
    in this function. Hash generating algorithm is taken from hash_add()
    function, while 256 lines table is used to store pending requests.  This
    fixes problem and improves the performance.
    
    Reported-by: Alexey Kuznetsov <kuznet@virtuozzo.com>
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -419,6 +419,9 @@ struct fuse_iqueue {
 	struct fasync_struct *fasync;
 };
 
+#define FUSE_PQ_HASH_BITS 8
+#define FUSE_PQ_HASH_SIZE (1 << FUSE_PQ_HASH_BITS)
+
 struct fuse_pqueue {
 	/** Connection established */
 	unsigned connected;
@@ -426,8 +429,8 @@ struct fuse_pqueue {
 	/** Lock protecting accessess to  members of this structure */
 	spinlock_t lock;
 
-	/** The list of requests being processed */
-	struct list_head processing;
+	/** Hash table of requests being processed */
+	struct list_head *processing;
 
 	/** The list of requests under I/O */
 	struct list_head io;
