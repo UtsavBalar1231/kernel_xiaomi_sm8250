b339704fc7c7b51a907d974d664ba99b9a1e365d
Paul E. McKenney <paulmck@linux.ibm.com>
2018-11-06 19:06:51 -0800

    UPSTREAM: lockdep: Replace synchronize_sched() with synchronize_rcu()
    Now that synchronize_rcu() waits for preempt-disable regions of code
    as well as RCU read-side critical sections, synchronize_sched() can be
    replaced by synchronize_rcu().  This commit therefore makes this change.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.ibm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Change-Id: Ie08ceaa2d1981dc7df63425c98bdffe188a78887
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/locking/lockdep.c
+++ kernel/locking/lockdep.c
@@ -4284,7 +4284,7 @@ void lockdep_free_key_range(void *start, unsigned long size)
 	 *
 	 * sync_sched() is sufficient because the read-side is IRQ disable.
 	 */
-	synchronize_sched();
+	synchronize_rcu();
 
 	/*
 	 * XXX at this point we could return the resources to the pool;
