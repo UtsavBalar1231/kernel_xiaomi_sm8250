527f8e055ae7ddef4a840355a01d3dd21ea7b84b
Connor O'Brien <connoro@google.com>
2018-12-05 12:31:26 -0800

    Revert "sched: fair: Always try to use energy efficient cpu for wakeups"
    This reverts commit 63c27502786646271b4c4ba32268b727e294bbb2.
    
    Bug: 117438867
    Bug: 144961676
    Test: Tracing confirms EAS is no longer always used
    Change-Id: If321547a86592527438ac21c3734a9f4decda712
    Signed-off-by: Connor O'Brien <connoro@google.com>
    Signed-off-by: Jimmy Shiu <jimmyshiu@google.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8101,16 +8101,6 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 	int want_affine = 0;
 	int sync = (wake_flags & WF_SYNC) && !(current->flags & PF_EXITING);
 
-	if (static_branch_unlikely(&sched_energy_present)) {
-		rcu_read_lock();
-		new_cpu = find_energy_efficient_cpu(p, prev_cpu, sync,
-						    sibling_count_hint);
-		if (unlikely(new_cpu < 0))
-			new_cpu = prev_cpu;
-		rcu_read_unlock();
-		return new_cpu;
-	}
-
 	if (sd_flag & SD_BALANCE_WAKE) {
 		record_wakee(p);
 
