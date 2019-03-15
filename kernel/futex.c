48a46eb44ff272c91694dc7fdb9a2db5c8d84937
Chen Jie <chenjie6@huawei.com>
2019-03-15 03:44:38 +0000

    UPSTREAM: futex: Ensure that futex address is aligned in handle_futex_death()
    The futex code requires that the user space addresses of futexes are 32bit
    aligned. sys_futex() checks this in futex_get_keys() but the robust list
    code has no alignment check in place.
    
    As a consequence the kernel crashes on architectures with strict alignment
    requirements in handle_futex_death() when trying to cmpxchg() on an
    unaligned futex address which was retrieved from the robust list.
    
    [ tglx: Rewrote changelog, proper sizeof() based alignement check and add
      	comment ]
    
    Fixes: 0771dfefc9e5 ("[PATCH] lightweight robust futexes: core")
    Signed-off-by: Chen Jie <chenjie6@huawei.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: <dvhart@infradead.org>
    Cc: <peterz@infradead.org>
    Cc: <zengweilin@huawei.com>
    Cc: stable@vger.kernel.org
    Link: https://lkml.kernel.org/r/1552621478-119787-1-git-send-email-chenjie6@huawei.com
    Change-Id: Ib6c9677e6194d591d3956da1422de38a5ef4b838
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/futex.c
+++ kernel/futex.c
@@ -3560,6 +3560,10 @@ static int handle_futex_death(u32 __user *uaddr, struct task_struct *curr,
 	if ((((unsigned long)uaddr) % sizeof(*uaddr)) != 0)
 		return -1;
 
+	/* Futex address must be 32bit aligned */
+	if ((((unsigned long)uaddr) % sizeof(*uaddr)) != 0)
+		return -1;
+
 retry:
 	if (get_user(uval, uaddr))
 		return -1;
