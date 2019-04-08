356acf17f01c611108e619947414ac89c557461f
Danny Lin <danny@kdrag0n.dev>
2019-04-07 23:34:36 -0700

    profiling: Implement a simple task exit notifier when disabled
    Some kernel code currently depends on the profiling subsystem solely for
    its task exit notifier. The rest of the profiling subsystem is left
    unused.
    
    Add a simple task exit notifier implemented in kernel/exit.c to make
    such code work properly even when profiling is disabled. This will allow
    unnecessary profiling code to be disabled without breakage.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -777,6 +777,32 @@ static void check_stack_usage(void)
 static inline void check_stack_usage(void) {}
 #endif
 
+#ifndef CONFIG_PROFILING
+static BLOCKING_NOTIFIER_HEAD(task_exit_notifier);
+
+int profile_event_register(enum profile_type t, struct notifier_block *n)
+{
+	if (t == PROFILE_TASK_EXIT)
+		return blocking_notifier_chain_register(&task_exit_notifier, n);
+
+	return -ENOSYS;
+}
+
+int profile_event_unregister(enum profile_type t, struct notifier_block *n)
+{
+	if (t == PROFILE_TASK_EXIT)
+		return blocking_notifier_chain_unregister(&task_exit_notifier,
+							  n);
+
+	return -ENOSYS;
+}
+
+void profile_task_exit(struct task_struct *tsk)
+{
+	blocking_notifier_call_chain(&task_exit_notifier, 0, tsk);
+}
+#endif
+
 void __noreturn do_exit(long code)
 {
 	struct task_struct *tsk = current;
