1f41c8fd0d70e4da625d183b4cb4e9be9fad7d59
Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
2019-04-08 18:27:00 -0600

    data-kernel: Add perf and shs modules as DLKM
    Required for .ko's to be part of vendor images
    and automatically signed with bootimage.
    
    CRs-Fixed: 2423695
    Change-Id: I280721f38b499f7ab0eabf0ea88afadb89212b38

---
+++ data_dlkm_vendor_board.mk
@@ -0,0 +1,8 @@
+#Build rmnet perf & shs
+DATA_DLKM_BOARD_PLATFORMS_LIST := msmnile
+DATA_DLKM_BOARD_PLATFORMS_LIST += kona
+
+ifeq ($(call is-board-platform-in-list,$(DATA_DLKM_BOARD_PLATFORMS_LIST)),true)
+BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/rmnet_shs.ko
+BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/rmnet_perf.ko
+endif
