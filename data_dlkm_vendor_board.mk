90c76228c779597fc2bbaf13cc86e5bf6f59d0dc
Vijayakumar Badiger <vbadig@codeaurora.org>
2019-05-08 21:01:48 -0700

    Excluded rmnet_perf and rmnet_shs on automtoive build
    Change-Id: I67ee777ec806d1f59b7d76289b9bc73f3f1246c6

--- data_dlkm_vendor_board.mk
+++ data_dlkm_vendor_board.mk
@@ -1,8 +1,10 @@
 #Build rmnet perf & shs
 DATA_DLKM_BOARD_PLATFORMS_LIST := msmnile
 DATA_DLKM_BOARD_PLATFORMS_LIST += kona
-
+ifneq ($(TARGET_BOARD_AUTO),true)
 ifeq ($(call is-board-platform-in-list,$(DATA_DLKM_BOARD_PLATFORMS_LIST)),true)
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/rmnet_shs.ko
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/rmnet_perf.ko
 endif
+endif
+
