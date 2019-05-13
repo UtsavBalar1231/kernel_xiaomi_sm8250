2922c259e175826de4c50b235834490d3ce4ae76
Maheshwar Ajja <majja@codeaurora.org>
2019-05-13 12:28:25 -0700

    msm: vidc: enable cvp usage
    Enable CVP usage to improve encoded video quality.
    
    Change-Id: Ice0b6590435c094fa52bedb89f65150ab0c9a378
    Signed-off-by: Maheshwar Ajja <majja@codeaurora.org>

--- msm/vidc/msm_vidc_debug.c
+++ msm/vidc/msm_vidc_debug.c
@@ -25,7 +25,7 @@ bool msm_vidc_fw_coverage = !true;
 bool msm_vidc_thermal_mitigation_disabled = !true;
 int msm_vidc_clock_voting = !1;
 bool msm_vidc_syscache_disable = !true;
-bool msm_vidc_cvp_usage = !true;
+bool msm_vidc_cvp_usage = true;
 
 #define MAX_DBG_BUF_SIZE 4096
 
