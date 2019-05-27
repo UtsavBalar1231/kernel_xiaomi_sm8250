260b19e5657fdcaa7e0fef26f10494da8fa60dae
Rick Edgecombe <rick.p.edgecombe@intel.com>
2019-05-27 14:10:58 -0700

    BACKPORT: mm/vmalloc: Avoid rare case of flushing TLB with weird arguments
    In a rare case, flush_tlb_kernel_range() could be called with a start
    higher than the end.
    
    In vm_remove_mappings(), in case page_address() returns 0 for all pages
    (for example they were all in highmem), _vm_unmap_aliases() will be
    called with start = ULONG_MAX, end = 0 and flush = 1.
    
    If at the same time, the vmalloc purge operation is triggered by something
    else while the current operation is between remove_vm_area() and
    _vm_unmap_aliases(), then the vm mapping just removed will be already
    purged. In this case the call of vm_unmap_aliases() may not find any other
    mappings to flush and so end up flushing start = ULONG_MAX, end = 0. So
    only set flush = true if we find something in the direct mapping that we
    need to flush, and this way this can't happen.
    
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: David S. Miller <davem@davemloft.net>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Meelis Roos <mroos@linux.ee>
    Cc: Nadav Amit <namit@vmware.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Fixes: 868b104d7379 ("mm/vmalloc: Add flag for freeing of special permsissions")
    Link: https://lkml.kernel.org/r/20190527211058.2729-3-rick.p.edgecombe@intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Change-Id: I491172fd43e04aa7ebc3da4ea3d84fe947f45e53

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -2128,6 +2128,7 @@ static void vm_remove_mappings(struct vm_struct *area, int deallocate_pages)
 {
 	unsigned long start = ULONG_MAX, end = 0;
 	int flush_reset = area->flags & VM_FLUSH_RESET_PERMS;
+	int flush_dmap = 0;
 	int i;
 
 	/*
@@ -2166,6 +2167,7 @@ static void vm_remove_mappings(struct vm_struct *area, int deallocate_pages)
 		if (addr) {
 			start = min(addr, start);
 			end = max(addr + PAGE_SIZE, end);
+			flush_dmap = 1;
 		}
 	}
 
@@ -2175,7 +2177,7 @@ static void vm_remove_mappings(struct vm_struct *area, int deallocate_pages)
 	 * reset the direct map permissions to the default.
 	 */
 	set_area_direct_map(area, set_direct_map_invalid_noflush);
-	_vm_unmap_aliases(start, end, 1);
+	_vm_unmap_aliases(start, end, flush_dmap);
 	set_area_direct_map(area, set_direct_map_default_noflush);
 }
 
