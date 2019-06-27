d9ceba876f5f9e6de84274f4bf8d9570552e66b6
Shi Zhongbo <zhongbos@codeaurora.org>
2019-06-27 16:20:36 +0800

    msm: vidc: refinements for lossless encoding
    1. Refine work mode to 2 and disable low latency by default;
    2. Refine output buffer size to (width * height) * 9 / 4;
    3. Double scratch buffer size for lossless encoding;
    
    Change-Id: If298b9fc63fc2329f02dcfcef5e4fd0667d3e8e6
    Signed-off-by: Shi Zhongbo <zhongbos@codeaurora.org>

--- msm/vidc/msm_vidc_clocks.c
+++ msm/vidc/msm_vidc_clocks.c
@@ -1514,11 +1514,9 @@ int msm_vidc_decide_work_mode_iris2(struct msm_vidc_inst *inst)
 			/* For WORK_MODE_1, set Low Latency mode by default */
 			latency.enable = true;
 		}
-		if (inst->rc_type == RATE_CONTROL_LOSSLESS &&
-			out_f->fmt.pix_mp.pixelformat == V4L2_PIX_FMT_H264) {
-			dprintk(VIDC_HIGH,
-				"Set work mode to low latency for AVC lossless encoding.");
-			latency.enable = true;
+		if (inst->rc_type == RATE_CONTROL_LOSSLESS) {
+			pdata.video_work_mode = HFI_WORKMODE_2;
+			latency.enable = false;
 		}
 	} else {
 		return -EINVAL;
