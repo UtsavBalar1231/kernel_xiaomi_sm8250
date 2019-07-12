98cd7daaa222ce176e50b4d3689f8a284730e48f
Geert Uytterhoeven <geert+renesas@glider.be>
2019-07-11 20:59:06 -0700

    BACKPORT: mm/vmalloc.c: spelling> s/informaion/information/
    Link: http://lkml.kernel.org/r/20190607113509.15032-1-geert+renesas@glider.be
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: Souptick Joarder <jrdr.linux@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Change-Id: Ibad0d575d99fbb3a7e9ee142c4c2418aebc33b96

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -2804,7 +2804,7 @@ static int aligned_vwrite(char *buf, char *addr, unsigned long count)
  *	Note: In usual ops, vread() is never necessary because the caller
  *	should know vmalloc() area is valid and can use memcpy().
  *	This is for routines which have to access vmalloc area without
- *	any informaion, as /dev/kmem.
+ *	any information, as /dev/kmem.
  *
  */
 
