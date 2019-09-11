8c91109b74c7c4dee048cc4b901a842b1a2a1f1d
Venkat Chinta <vchinta@codeaurora.org>
2019-09-11 15:00:07 -0700

    msm: camera: core: Change return type
    Return different error number when new requests and update
    packets are rejected due to bad request ID. This allows
    userspace to differentiate this specific reason for failure.
    
    CRs-Fixed: 2518451
    Change-Id: I0400c6f2b2e0baf99bee4b4a3736bb1eab646627
    Signed-off-by: Venkat Chinta <vchinta@codeaurora.org>

--- drivers/cam_req_mgr/cam_req_mgr_core.c
+++ drivers/cam_req_mgr/cam_req_mgr_core.c
@@ -3209,7 +3209,7 @@ int cam_req_mgr_schedule_request(
 		CAM_INFO(CAM_CRM,
 			"request %lld is flushed, last_flush_id to flush %d",
 			sched_req->req_id, link->last_flush_id);
-		rc = -EINVAL;
+		rc = -EBADR;
 		goto end;
 	}
 
