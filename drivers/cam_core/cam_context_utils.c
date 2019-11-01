15feb22bdd685e1f0b09b0cb14e3021cc7b19dbb
Karthik Jayakumar <kjayakum@codeaurora.org>
2019-11-01 14:59:32 -0700

    msm: camera: core: Fix extraneous variable declaration
    Remove an extra variable declaration within a for loop.
    
    CRs-Fixed: 2554484
    Change-Id: I89885eeb8f89893ad7054d54a694a040e4c0bfbb
    Signed-off-by: Karthik Jayakumar <kjayakum@codeaurora.org>

--- drivers/cam_core/cam_context_utils.c
+++ drivers/cam_core/cam_context_utils.c
@@ -481,7 +481,7 @@ int32_t cam_context_prepare_dev_to_hw(struct cam_context *ctx,
 
 	return rc;
 put_ctx_ref:
-	for (j; j >= 0; j--)
+	for (; j >= 0; j--)
 		cam_context_putref(ctx);
 put_ref:
 	for (--i; i >= 0; i--) {
