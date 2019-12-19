61ad8a9cbfea3af4691062fdb1cd51ab5b59b1de
zengyonggang <zengyonggang@xiaomi.com>
2019-12-19 16:24:52 +0800

    audio: asoc: fix compress volume can't take effect issue
    PICASSO-4874
    
    compress volume can't take effect issue
    
    Change-Id: I9a10b5112313e87a7761e9c8ed1451e696b684b1
    Signed-off-by: lugandong <lugandong@xiaomi.com>

--- asoc/msm-compress-q6-v2.c
+++ asoc/msm-compress-q6-v2.c
@@ -440,8 +440,8 @@ static int msm_compr_set_volume(struct snd_compr_stream *cstream,
 		gain_list[0] = volume_l;
 		gain_list[1] = volume_r;
 		gain_list[2] = volume_l;
-		if (use_default)
-			num_channels = 3;
+		num_channels = 3;
+		use_default = true;
 		rc = q6asm_set_multich_gain(prtd->audio_client, num_channels,
 					gain_list, chmap, use_default);
 	}
