f5f28a7b7285e020dbc051f8583f28495a040811
Amir Vajid <avajid@codeaurora.org>
2020-01-22 11:09:19 -0800

    PM / devfreq: Make governor registration less verbose
    Several devfreq governors print to kernel logs for each
    device that registers. Update them to only print when
    debugging is enabled.
    
    Change-Id: Id7cf8c9ef70363cdfd4fc1fc72b109f28ab07288
    Signed-off-by: Amir Vajid <avajid@codeaurora.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/devfreq/governor_cache_hwmon.c
+++ drivers/devfreq/governor_cache_hwmon.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2014-2015, 2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2015, 2019-2020, The Linux Foundation. All rights reserved.
  */
 
 #define pr_fmt(fmt) "cache-hwmon: " fmt
@@ -401,7 +401,7 @@ int register_cache_hwmon(struct device *dev, struct cache_hwmon *hwmon)
 	mutex_unlock(&register_lock);
 
 	if (!ret) {
-		dev_info(dev, "Cache HWmon governor registered.\n");
+		dev_dbg(dev, "Cache HWmon governor registered.\n");
 	} else {
 		dev_err(dev, "Failed to add Cache HWmon governor\n");
 		return ret;
