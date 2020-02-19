8310858adcc42e8803abe16f899bd2b1d436229e
Sravan Goud <sgoud@codeaurora.org>
2020-02-19 16:28:44 +0530

    qcacmn: In case of peer reuse set valid flag
    Currently if peer reference is reused valid flag is not set.
    In peer delete function if valid flag is not set, function
    returns with out decrementing the ref count and also not
    releasing the local id. This leads to local id exhaustion if
    back to back peer reuse happens which is common in case of back
    to back roaming to same AP. As a result connection to AP fails
    due to unavailability of local id. In this change set valid flag
    if peer reference is reused.
    
    Change-Id: Ibeccbc4d33aac9e4285b48a4b8abdd2276fd7ede
    CRs-Fixed: 2624798

--- dp/wifi3.0/dp_main.c
+++ dp/wifi3.0/dp_main.c
@@ -5490,6 +5490,7 @@ dp_peer_create_wifi3(struct cdp_soc_t *soc_hdl, uint8_t vdev_id,
 				vdev->opmode);
 		}
 
+		peer->valid = 1;
 		dp_local_peer_id_alloc(pdev, peer);
 
 		qdf_spinlock_create(&peer->peer_info_lock);
