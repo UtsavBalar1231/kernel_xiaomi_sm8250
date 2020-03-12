700983517f0c6534bca2c2d569fa541ee304ea9a
Laxminath Kasam <lkasam@codeaurora.org>
2020-03-12 17:40:47 +0530

    asoc: wcd938x: Add vdd-buck as on demand supply
    On lagoon, vdd-buck need to be dynamic supply.
    Add required support to handle buck as dynamic supply.
    
    Change-Id: If6c27fffe0d0c44f178e61a6eae6ef63775c8988
    Signed-off-by: Laxminath Kasam <lkasam@codeaurora.org>

--- include/asoc/msm-cdc-supply.h
+++ include/asoc/msm-cdc-supply.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
-/* Copyright (c) 2016, 2018-2019 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2016, 2018-2020 The Linux Foundation. All rights reserved.
  */
 
 #ifndef __CODEC_POWER_SUPPLY_H__
@@ -21,6 +21,10 @@ struct cdc_regulator {
 extern int msm_cdc_get_power_supplies(struct device *dev,
 				      struct cdc_regulator **cdc_vreg,
 				      int *total_num_supplies);
+extern bool msm_cdc_is_ondemand_supply(struct device *dev,
+				struct regulator_bulk_data *supplies,
+				struct cdc_regulator *cdc_vreg,
+				int num_supplies, char *supply_name);
 extern int msm_cdc_disable_ondemand_supply(struct device *dev,
 					struct regulator_bulk_data *supplies,
 					struct cdc_regulator *cdc_vreg,
