ee28fdbbcb3512d8c2a59118af408401af1aae8f
Lei Chen <chenlei@codeaurora.org>
2020-03-13 23:21:21 +0800

    disp: msm: Subtract DSI interrupt count after interrupt was destroyed
    DSI interrupt may be destroyed before it is disabled, it will cause to
    the interrupt count can't be cleared, so subtrace DSI interrupt count
    in disable function even it was destroyed.
    
    Change-Id: I430b0281957db588c7405d5775d0c10f2f498b36
    Signed-off-by: Lei Chen <chenlei@codeaurora.org>

--- msm/dsi/dsi_ctrl.c
+++ msm/dsi/dsi_ctrl.c
@@ -2733,8 +2733,7 @@ void dsi_ctrl_disable_status_interrupt(struct dsi_ctrl *dsi_ctrl,
 {
 	unsigned long flags;
 
-	if (!dsi_ctrl || dsi_ctrl->irq_info.irq_num == -1 ||
-			intr_idx >= DSI_STATUS_INTERRUPT_COUNT)
+	if (!dsi_ctrl || intr_idx >= DSI_STATUS_INTERRUPT_COUNT)
 		return;
 
 	SDE_EVT32(dsi_ctrl->cell_index, SDE_EVTLOG_FUNC_ENTRY);
@@ -2747,7 +2746,8 @@ void dsi_ctrl_disable_status_interrupt(struct dsi_ctrl *dsi_ctrl,
 					dsi_ctrl->irq_info.irq_stat_mask);
 
 			/* don't need irq if no lines are enabled */
-			if (dsi_ctrl->irq_info.irq_stat_mask == 0)
+			if (dsi_ctrl->irq_info.irq_stat_mask == 0 &&
+				dsi_ctrl->irq_info.irq_num != -1)
 				disable_irq_nosync(dsi_ctrl->irq_info.irq_num);
 		}
 
