2ec044eec31dade2be625368488a0a524bc439a2
Samantha Tran <samtran@codeaurora.org>
2020-03-19 11:36:39 -0700

    disp: msm: sde: correct line time to include DSC
    Current computation of line time does not include compression ratio from
    DSC. This change computes compression ratio during any mode change
    and stores it in sde_crtc.
    
    Change-Id: Ib1e045dce17fcf006447d4562b402cc3f214ed8c
    Signed-off-by: Samantha Tran <samtran@codeaurora.org>

--- msm/sde/sde_encoder.c
+++ msm/sde/sde_encoder.c
@@ -2915,6 +2915,8 @@ static void sde_encoder_virt_mode_set(struct drm_encoder *drm_enc,
 	/* store the mode_info */
 	sde_connector_state_get_mode_info(conn->state, &sde_enc->mode_info);
 
+	sde_crtc_set_compression_ratio(sde_enc->mode_info, sde_enc->crtc);
+
 	/* release resources before seamless mode change */
 	if (msm_is_mode_seamless_dms(adj_mode) ||
 			(msm_is_mode_seamless_dyn_clk(adj_mode) &&
