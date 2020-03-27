9e8e0f6d803c5cd744df8edce965bb1c8a1866ce
Aditya Bavanari <abavanar@codeaurora.org>
2020-03-27 15:10:12 +0530

    config: Enable digital codec resource manager config
    Enable digital codec resource manager config for kona,
    lito and bengal targets.
    
    Change-Id: Ie41a9e61032d20d4deaa95b0669c509ff9b14968
    Signed-off-by: Aditya Bavanari <abavanar@codeaurora.org>

--- config/bengalautoconf.h
+++ config/bengalautoconf.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
-/* Copyright (c) 2019, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2019-2020, The Linux Foundation. All rights reserved.
  */
 
 #define CONFIG_PINCTRL_LPI 1
@@ -31,6 +31,7 @@
 #define CONFIG_VA_MACRO 1
 #define CONFIG_RX_MACRO 1
 #define CONFIG_TX_MACRO 1
+#define CONFIG_DIGITAL_CDC_RSC_MGR 1
 #define CONFIG_SND_SOC_WCD_IRQ 1
 #define CONFIG_SND_SOC_WCD937X 1
 #define CONFIG_SND_SOC_WCD937X_SLAVE 1
