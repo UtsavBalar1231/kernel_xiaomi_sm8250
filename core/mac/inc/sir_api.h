0a59037349313b1a3b69f7d0b5d94da69bf64514
Aditya Kodukula <akodukul@codeaurora.org>
2020-04-15 13:30:14 -0700

    qcacld-3.0: Add a feature flag WLAN_POWER_DEBUG for power stats
    Power stats is moved from debugfs to sysfs, and hence add the feature
    flag WLAN_POWER_DEBUG such that this feature can be compiled out for
    low memory foot print drivers.
    
    Change-Id: Ic395f393580fcd59faa4e4676f67a39ff37bdd1a
    CRs-Fixed: 2665089

--- core/mac/inc/sir_api.h
+++ core/mac/inc/sir_api.h
@@ -3341,6 +3341,7 @@ struct auto_shutdown_cmd {
 };
 #endif
 
+#ifdef WLAN_POWER_DEBUG
 /**
  * struct power_stats_response - Power stats response
  * @cumulative_sleep_time_ms: cumulative sleep time in ms
@@ -3360,6 +3361,7 @@ struct power_stats_response {
 	uint32_t num_debug_register;
 	uint32_t *debug_registers;
 };
+#endif
 
 #ifdef WLAN_FEATURE_BEACON_RECEPTION_STATS
 #define MAX_BCNMISS_BITMAP 8
