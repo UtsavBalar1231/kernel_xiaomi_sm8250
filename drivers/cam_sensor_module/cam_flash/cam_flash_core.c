c8a42818735eda5a3ea73832881ae131158b4ecd
Anil Kumar Kanakanti <akanakan@codeaurora.org>
2020-04-17 22:52:58 +0530

    msm: camera: sensor: Turn off the flash while flushing
    Turn off the flash while flushing if the flushed request is
    flash off and removed flush request ID validation in flush_all
    scenario.
    
    CRs-Fixed: 2667472
    Change-Id: Iaf123f2f56117d7b7d1ef6d2291b0cde8c232d44
    Signed-off-by: Anil Kumar Kanakanti <akanakan@codeaurora.org>

--- drivers/cam_sensor_module/cam_flash/cam_flash_core.c
+++ drivers/cam_sensor_module/cam_flash/cam_flash_core.c
@@ -303,7 +303,6 @@ int cam_flash_pmic_flush_request(struct cam_flash_ctrl *fctrl,
 			if ((flash_data->opcode ==
 				CAMERA_SENSOR_FLASH_OP_OFF) &&
 				(flash_data->cmn_attr.request_id > 0) &&
-				(flash_data->cmn_attr.request_id <= req_id) &&
 				flash_data->cmn_attr.is_settings_valid) {
 				is_off_needed = true;
 				CAM_DBG(CAM_FLASH,
@@ -504,7 +503,7 @@ int cam_flash_off(struct cam_flash_ctrl *flash_ctrl)
 		CAM_ERR(CAM_FLASH, "Flash control Null");
 		return -EINVAL;
 	}
-
+	CAM_DBG(CAM_FLASH, "Flash OFF Triggered");
 	if (flash_ctrl->switch_trigger)
 		cam_res_mgr_led_trigger_event(flash_ctrl->switch_trigger,
 			(enum led_brightness)LED_SWITCH_OFF);
@@ -1552,6 +1551,8 @@ int cam_flash_pmic_pkt_parser(struct cam_flash_ctrl *fctrl, void *arg)
 				flash_data->led_current_ma[i]
 				= flash_operation_info->led_current_ma[i];
 
+			CAM_DBG(CAM_FLASH,
+				"FLASH_CMD_TYPE op:%d", flash_data->opcode);
 			if (flash_data->opcode == CAMERA_SENSOR_FLASH_OP_OFF)
 				add_req.skip_before_applying |= SKIP_NEXT_FRAME;
 		}
