b18fef4e85b9691a561c66e3745c98341a5431d7
Srinivas Dasari <dasaris@codeaurora.org>
2020-04-23 10:55:22 +0530

    qcacld-3.0: Consider host and firmware capa for NAN separate vdev
    Firmware advertises the NAN separate vdev capability through
    the service capability wmi_service_nan_vdev and host advertises
    through the ini param nan_separate_iface_support. Both of the
    capabilities must be checked to support the NAN separate vdev
    support. Define an API to check both the capabilities and made
    the individual capability APIs static. So that individual APIs
    are not accessed outside this common API. Use this API to
    know the support. This is to avoid any possible misses in
    checking the support.
    For example, NAN vdev id is fetched from the NAN enable
    response message received from firmware only based on
    firmware capability in nan_handle_enable_rsp. If firmware
    supports the feature and ini is set to 0, then vdev id might
    be invalid as it's fetched from the NAN enable response
    without considering host capability. If either firmware or
    host doesn't support NAN separate vdev feature, firmware
    creates vdev and it may not fill the vdev id in NAN enable
    response. Host shall use NAN_PSEUDO_VDEV_ID then.
    So, consider NAN separate vdev feature as supported only if
    both host and firmware support.
    Also fetch the firmware capability to support NAN separate vdev
    when wma_rx_service_ready_ext_event is received and update to NAN
    psoc priv object.
    
    Change-Id: I50e76fbe17befb28a5262fc26f5675b67f4d21f2
    CRs-Fixed: 2650354

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -13212,8 +13212,7 @@ hdd_open_adapters_for_mission_mode(struct hdd_context *hdd_ctx)
 	 * Create separate interface (wifi-aware0) for NAN. All NAN commands
 	 * should go on this new interface.
 	 */
-	if (ucfg_nan_is_vdev_creation_allowed(hdd_ctx->psoc) &&
-	    ucfg_nan_get_is_separate_nan_iface(hdd_ctx->psoc)) {
+	if (ucfg_nan_is_vdev_creation_allowed(hdd_ctx->psoc)) {
 		mac_addr = wlan_hdd_get_intf_addr(hdd_ctx, QDF_NAN_DISC_MODE);
 		if (!mac_addr)
 			goto err_close_adapters;
