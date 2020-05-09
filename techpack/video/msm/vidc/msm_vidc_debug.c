d936d178d0a7058b3e760cdd58041a7df35293bf
franciscofranco <franciscofranco.1990@gmail.com>
2020-05-09 22:43:07 +0200

    msm: vidc: disable debug logs
    Signed-off-by: LibXZR <xzr467706992@163.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- techpack/video/msm/vidc/msm_vidc_debug.c
+++ techpack/video/msm/vidc/msm_vidc_debug.c
@@ -10,8 +10,7 @@
 #include "vidc_hfi_api.h"
 #include <linux/of_fdt.h>
 
-int msm_vidc_debug = VIDC_ERR | VIDC_PRINTK |
-	FW_ERROR | FW_FATAL | FW_FTRACE;
+int msm_vidc_debug = 0;
 EXPORT_SYMBOL(msm_vidc_debug);
 
 bool msm_vidc_lossless_encode = !true;
