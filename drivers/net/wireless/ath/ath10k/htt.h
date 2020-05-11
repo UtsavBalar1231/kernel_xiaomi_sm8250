1f4f6eee538beadd6bf822583cdf8c922fa6cfbd
Arnd Bergmann <arnd@arndb.de>
2020-05-11 15:53:35 +0300

    BACKPORT: ath10k: fix gcc-10 zero-length-bounds warnings
    gcc-10 started warning about out-of-bounds access for zero-length
    arrays:
    
    In file included from drivers/net/wireless/ath/ath10k/core.h:18,
                     from drivers/net/wireless/ath/ath10k/htt_rx.c:8:
    drivers/net/wireless/ath/ath10k/htt_rx.c: In function 'ath10k_htt_rx_tx_fetch_ind':
    drivers/net/wireless/ath/ath10k/htt.h:1683:17: warning: array subscript 65535 is outside the bounds of an interior zero-length array 'struct htt_tx_fetch_record[0]' [-Wzero-length-bounds]
     1683 |  return (void *)&ind->records[le16_to_cpu(ind->num_records)];
          |                 ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    drivers/net/wireless/ath/ath10k/htt.h:1676:29: note: while referencing 'records'
     1676 |  struct htt_tx_fetch_record records[0];
          |                             ^~~~~~~
    
    Make records[] a flexible array member to allow this, moving it behind
    the other zero-length member that is not accessed in a way that gcc
    warns about.
    
    Fixes: 22e6b3bc5d96 ("ath10k: add new htt definitions")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20200509120707.188595-1-arnd@arndb.de
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Change-Id: I3bde7e7b9ca7edd978271d32226b2021eec999ef
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/net/wireless/ath/ath10k/htt.h
+++ drivers/net/wireless/ath/ath10k/htt.h
@@ -1502,8 +1502,8 @@ struct htt_tx_fetch_ind {
 	__le32 token;
 	__le16 num_resp_ids;
 	__le16 num_records;
-	struct htt_tx_fetch_record records[0];
 	__le32 resp_ids[0]; /* ath10k_htt_get_tx_fetch_ind_resp_ids() */
+	struct htt_tx_fetch_record records[];
 } __packed;
 
 static inline void *
