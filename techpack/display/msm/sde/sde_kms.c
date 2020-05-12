4212dabf52ec114e49214dfb2e59f8af2c0c6c4b
Yuan Zhao <yzhao@codeaurora.org>
2020-05-12 14:44:03 +0800

    BACKPORT: disp: msm: debugfs interface for sde connector to do DSI read
    This change implements a new feature to read the cmds response
    of the panel from sde connector interface. Sde connector opens
    debugfs interface for all the connectors those have support for
    cmd receive operation.
    
    Sde connector init module creates rx_cmd debugfs file at
    /<debugfs-root>/dri/0/DSI-1/ for DSI-1 connector.
    Format for DSI command transfer:
    echo "command bytes" > /<debugfs-root/dri/0/DSI-1/rx_cmd
    byte-0: the length of received buffer
    byte-1: data-type
    byte-2: last command. always 0x01
    byte-3: channel number
    byte-4: flags. MIPI_DSI_MSG_*, must be set to 0xa
    byte-5: 0x00
    byte-6 and byte-7: command payload length
    byte-8 to byte-[8+payload length]: command payload
    Example:
    echo "0x01 0x06 0x01 0x00 0x0a 0x00 0x00 0x01 0x0a" > rx_cmd
    The command receive operations are allowed only if controller
    (ex. DSI controller) is in active state.
    Read the value of panel response:
    cat /<debugfs-root>/dri/0/DSI-1/rx_cmd
    returns the value of this command.
    nothing - failure, xx xx - success.
    
    Change-Id: I912b65d606e248c7a886d219f4363bf7766ee7b6
    Signed-off-by: Yuan Zhao <yzhao@codeaurora.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- techpack/display/msm/sde/sde_kms.c
+++ techpack/display/msm/sde/sde_kms.c
@@ -1435,6 +1435,7 @@ static int _sde_kms_setup_displays(struct drm_device *dev,
 		.get_panel_vfp = dsi_display_get_panel_vfp,
 		.get_default_lms = dsi_display_get_default_lms,
 		.get_qsync_min_fps = dsi_display_get_qsync_min_fps,
+		.cmd_receive = dsi_display_cmd_receive,
 	};
 	static const struct sde_connector_ops wb_ops = {
 		.post_init =    sde_wb_connector_post_init,
@@ -1450,6 +1451,7 @@ static int _sde_kms_setup_displays(struct drm_device *dev,
 		.cmd_transfer = NULL,
 		.cont_splash_config = NULL,
 		.get_panel_vfp = NULL,
+		.cmd_receive = NULL,
 	};
 	static const struct sde_connector_ops dp_ops = {
 		.set_info_blob = dp_connnector_set_info_blob,
@@ -1468,6 +1470,7 @@ static int _sde_kms_setup_displays(struct drm_device *dev,
 		.cont_splash_config = NULL,
 		.get_panel_vfp = NULL,
 		.update_pps = dp_connector_update_pps,
+		.cmd_receive = NULL,
 	};
 	struct msm_display_info info;
 	struct drm_encoder *encoder;
