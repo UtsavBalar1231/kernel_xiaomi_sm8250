efdb3934ec7eb83731e3c930b8d5948b05c1221e
Prasad Kumpatla <nkumpat@codeaurora.org>
2020-05-13 18:55:32 +0530

    asoc: codec: Add pre ssr up event for all macros
    Add a pre ssr up event in all macros ,where GFMUX reset
    to done before set the dev_up flag to true.
    
    Change-Id: I77229ccd0ed68aac841146a89fe7f76961260aea
    Signed-off-by: Prasad Kumpatla <nkumpat@codeaurora.org>

--- asoc/codecs/bolero/rx-macro.c
+++ asoc/codecs/bolero/rx-macro.c
@@ -1388,11 +1388,7 @@ static int rx_macro_event_handler(struct snd_soc_component *component,
 			}
 		}
 		break;
-	case BOLERO_MACRO_EVT_SSR_UP:
-		trace_printk("%s, enter SSR up\n", __func__);
-		rx_priv->dev_up = true;
-		/* reset swr after ssr/pdr */
-		rx_priv->reset_swr = true;
+	case BOLERO_MACRO_EVT_PRE_SSR_UP:
 		/* enable&disable RX_CORE_CLK to reset GFMUX reg */
 		ret = bolero_clk_rsc_request_clock(rx_priv->dev,
 						rx_priv->default_clk_id,
@@ -1405,6 +1401,12 @@ static int rx_macro_event_handler(struct snd_soc_component *component,
 			bolero_clk_rsc_request_clock(rx_priv->dev,
 						rx_priv->default_clk_id,
 						RX_CORE_CLK, false);
+		break;
+	case BOLERO_MACRO_EVT_SSR_UP:
+		trace_printk("%s, enter SSR up\n", __func__);
+		rx_priv->dev_up = true;
+		/* reset swr after ssr/pdr */
+		rx_priv->reset_swr = true;
 
 		if (rx_priv->swr_ctrl_data)
 			swrm_wcd_notify(
