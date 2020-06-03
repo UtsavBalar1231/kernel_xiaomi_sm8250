cee9cf1aab6f854d30ac94bc3125b30addb373fe
Johannes Weiner <hannes@cmpxchg.org>
2020-06-03 16:02:37 -0700

    mm: allow swappiness that prefers reclaiming anon over the file workingset
    With the advent of fast random IO devices (SSDs, PMEM) and in-memory swap
    devices such as zswap, it's possible for swap to be much faster than
    filesystems, and for swapping to be preferable over thrashing filesystem
    caches.
    
    Allow setting swappiness - which defines the rough relative IO cost of
    cache misses between page cache and swap-backed pages - to reflect such
    situations by making the swap-preferred range configurable.
    
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Rik van Riel <riel@surriel.com>
    Link: http://lkml.kernel.org/r/20200520232525.798933-4-hannes@cmpxchg.org
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Change-Id: I94a65f8627c7692983f955dd9e687a7ce2f5693a
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -176,7 +176,7 @@ int kswapd_threads_current = DEF_KSWAPD_THREADS_PER_NODE;
 #endif
 
 /*
- * From 0 .. 100.  Higher means more swappy.
+ * From 0 .. 200.  Higher means more swappy.
  */
 int vm_swappiness = 60;
 /*
