07270289029899c69523b8db758f9f510e9136c5
Zou Wei <zou_wei@huawei.com>
2020-06-04 16:49:46 -0700

    mm: use false for bool variable
    Fixes coccicheck warnings:
    
      mm/zbud.c:246:1-20: WARNING: Assignment of 0/1 to bool variable
      mm/mremap.c:777:2-8: WARNING: Assignment of 0/1 to bool variable
      mm/huge_memory.c:525:9-10: WARNING: return of 0/1 in function 'is_transparent_hugepage' with return type bool
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Zou Wei <zou_wei@huawei.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Link: http://lkml.kernel.org/r/1586835930-47076-1-git-send-email-zou_wei@huawei.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- mm/mremap.c
+++ mm/mremap.c
@@ -770,7 +770,7 @@ SYSCALL_DEFINE5(mremap, unsigned long, addr, unsigned long, old_len,
 out:
 	if (offset_in_page(ret)) {
 		vm_unacct_memory(charged);
-		locked = 0;
+		locked = false;
 	}
 	up_write(&current->mm->mmap_sem);
 	if (locked && new_len > old_len)
