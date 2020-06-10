b362fde646f17304a3cf2fadafd51a23d82fc290
Utkarsh Bhatnagar <ubhatnag@codeaurora.org>
2020-06-11 02:48:09 +0530

    qcacld-3.0: Change prior conditions required for chainmask
    Change the prior conditions required for configuring Rx Chainmask &
    Tx Chainmask.
    Below conditions should be met for setting chainmask:
    1. Antenna sharing should be disabled.
    2. If gEnable2x2 is 1 then enableBTChainSeparation must be 1
       else gEnable2x2 should be 0.
    3. DBS should be disabled (gDualMacFeatureDisable = 1)
       or HW should be DBS 2x2 capable
    4. set TX/RX chainmask 3 in ini (gSetTxChainmask1x1/gSetRxChainmask1x1)
    5. HW should support TX/RX chainmask as 3
    
    Change-Id: If83c55af47abb591cdf1b5d2e3981d80bc305bf2
    CRs-Fixed: 2705520

--- components/mlme/dispatcher/src/wlan_mlme_api.c
+++ components/mlme/dispatcher/src/wlan_mlme_api.c
@@ -939,8 +939,8 @@ QDF_STATUS wlan_mlme_configure_chain_mask(struct wlan_objmgr_psoc *psoc,
 			  mlme_obj->cfg.chainmask_cfg.tx_chain_mask_5g,
 			  mlme_obj->cfg.chainmask_cfg.rx_chain_mask_5g);
 
-	if (enable2x2 || !enable_bt_chain_sep || as_enabled ||
-	   (!hw_dbs_2x2_cap && dual_mac_feature != DISABLE_DBS_CXN_AND_SCAN)) {
+	if ((enable2x2 && !enable_bt_chain_sep) || as_enabled ||
+	    (!hw_dbs_2x2_cap && dual_mac_feature != DISABLE_DBS_CXN_AND_SCAN)) {
 		mlme_legacy_debug("Cannot configure chainmask to FW");
 		return QDF_STATUS_E_FAILURE;
 	}
