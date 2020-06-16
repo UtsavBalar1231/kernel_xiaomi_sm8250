069ada4b23317bb8e23658f1fe4fcd956ea4992b
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-06-16 18:04:44 +0800

    zram: Implement xiaomi zRAM memory tracking feature
    Change-Id: I760cca12eb36a9dcf9b931f294f5a7e43a519777
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -41,7 +41,11 @@
  * The lower ZRAM_FLAG_SHIFT bits is for object size (excluding header),
  * the higher bits is for zram_pageflags.
  */
+#if IS_ENABLED(CONFIG_MIUI_ZRAM_MEMORY_TRACKING)
+#define ZRAM_FLAG_SHIFT (PAGE_SHIFT + 1)
+#else
 #define ZRAM_FLAG_SHIFT 24
+#endif
 
 /* Flags for zram pages (table[page_no].flags) */
 enum zram_pageflags {
@@ -55,6 +59,17 @@ enum zram_pageflags {
 
 	__NR_ZRAM_PAGEFLAGS,
 };
+
+#if IS_ENABLED(CONFIG_MIUI_ZRAM_MEMORY_TRACKING)
+#define ZRAM_WB_IDLE_SHIFT (__NR_ZRAM_PAGEFLAGS)
+
+#define ZRAM_WB_IDLE_BITS_LEN (4U)
+
+#define ZRAM_WB_IDLE_MIN (1U)
+#define ZRAM_WB_IDLE_MAX (10U)
+
+#define ZRAM_WB_IDLE_DEFAULT ZRAM_WB_IDLE_MIN
+#endif
 
 /*-- Data structures */
 
@@ -73,7 +88,7 @@ struct zram_table_entry {
 		unsigned long element;
 	};
 	unsigned long flags;
-#ifdef CONFIG_ZRAM_MEMORY_TRACKING
+#if defined(CONFIG_ZRAM_MEMORY_TRACKING) || IS_ENABLED(CONFIG_MIUI_ZRAM_MEMORY_TRACKING)
 	ktime_t ac_time;
 #endif
 };
@@ -92,10 +107,16 @@ struct zram_stats {
 	atomic_long_t max_used_pages;	/* no. of maximum pages stored */
 	atomic64_t writestall;		/* no. of write slow paths */
 	atomic64_t miss_free;		/* no. of missed free */
-#ifdef	CONFIG_ZRAM_WRITEBACK
+#ifdef  CONFIG_ZRAM_WRITEBACK
 	atomic64_t bd_count;		/* no. of pages in backing device */
 	atomic64_t bd_reads;		/* no. of reads from backing device */
 	atomic64_t bd_writes;		/* no. of writes from backing device */
+#if IS_ENABLED(CONFIG_MIUI_ZRAM_MEMORY_TRACKING)
+	atomic64_t wb_pages_max;	/* no. of max pages in backing device */
+#endif
+#endif
+#if IS_ENABLED(CONFIG_MIUI_ZRAM_MEMORY_TRACKING)
+	atomic64_t origin_pages_max;	/* no. of maximum origin pages stored */
 #endif
 	atomic64_t dup_data_size;	/*
 					 * compressed size of pages
@@ -108,6 +129,15 @@ struct zram_hash {
 	spinlock_t lock;
 	struct rb_root rb_root;
 };
+
+#if IS_ENABLED(CONFIG_MIUI_ZRAM_MEMORY_TRACKING)
+struct zram_pages_life {
+	unsigned int time_nr;
+	int *time_list;
+	unsigned long *lifes;
+	struct rcu_head rcu;
+};
+#endif
 
 struct zram {
 	struct zram_table_entry *table;
@@ -147,6 +177,12 @@ struct zram {
 #endif
 #ifdef CONFIG_ZRAM_MEMORY_TRACKING
 	struct dentry *debugfs_dir;
+#endif
+#if IS_ENABLED(CONFIG_MIUI_ZRAM_MEMORY_TRACKING)
+	struct zram_pages_life __rcu *pages_life;
+	ktime_t first_time;
+	ktime_t last_time;
+	atomic64_t avg_size;
 #endif
 };
 
