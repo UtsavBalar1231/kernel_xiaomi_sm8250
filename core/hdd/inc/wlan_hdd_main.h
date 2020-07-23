f3faf8be498e728a54419ebc67d434dd49c70673
Yeshwanth Sriram Guntuka <ysriramg@codeaurora.org>
2020-07-23 19:52:25 +0530

    qcacld-3.0: Add support to control GRO per vdev at runtime
    Add support to enable or disable GRO per adapter at
    runtime based on the ingress qdisc registered for the
    net device. Disable GRO when ingress qdisc is configured
    and enable GRO when there is no ingress qdisc configured.
    
    Change-Id: Ie9ced2c92d176c040bc44da4cee93db55c6a335a
    CRs-Fixed: 2739414

--- core/hdd/inc/wlan_hdd_main.h
+++ core/hdd/inc/wlan_hdd_main.h
@@ -1083,6 +1083,8 @@ struct hdd_context;
  *			   by hdd_netdev_feature_update
  * @netdev_features_update_work: work for handling the netdev features update
 				 for the adapter.
+ * @gro_disallowed: Flag to check if GRO is enabled or disable for adapter
+ * @gro_flushed: Flag to indicate if GRO explicit flush is done or not
  */
 struct hdd_adapter {
 	/* Magic cookie for adapter sanity verification.  Note that this
@@ -1382,6 +1384,8 @@ struct hdd_adapter {
 	bool handle_feature_update;
 
 	qdf_work_t netdev_features_update_work;
+	uint8_t gro_disallowed[DP_MAX_RX_THREADS];
+	uint8_t gro_flushed[DP_MAX_RX_THREADS];
 };
 
 #define WLAN_HDD_GET_STATION_CTX_PTR(adapter) (&(adapter)->session.station)
