1b2abce0b31c43af8d1c439364d3e5ee9ded7703
raychi <raychi@google.com>
2020-07-24 14:58:34 +0800

    usb: dwc3: skip trbs if DWC3_EP_TRANSFER_STARTED is clear
    If DWC3_EP_TRANSFER_STARTED is clear, it will do the giveback
    without skip trbs. It will cause the trb index wrong
    for next request. Therefore, add dwc3_gadget_ep_skip_trbs
    here to avoid this problem.
    
    Bug: 160373605
    
    Signed-off-by: raychi <raychi@google.com>
    Change-Id: I0ae79667fb46d3a298f4f67c9f461141b2ad9e79

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1871,6 +1871,7 @@ static int dwc3_gadget_ep_dequeue(struct usb_ep *ep,
 	}
 
 	dbg_ep_dequeue(dep->number, req);
+	dwc3_gadget_ep_skip_trbs(dep, req);
 	dwc3_gadget_giveback(dep, req, -ECONNRESET);
 
 out0:
