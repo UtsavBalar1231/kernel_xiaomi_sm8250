00be65d372e2766b819778fdac197e044a4ed72f
Jinwei Chen <jinweic@codeaurora.org>
2020-09-11 15:00:48 +0800

    qcacmn: Retry reo_dst_ctrl register writing if fails
    If reo_dst_ctrl register writing failed, this is a fatal error for
    IPA pipe going to down case as RX frames will still be routed to
    IPA rings then hit NOC error. retry register writing to see any
    chance to write successfully, if fail always, trigger SSR or panic.
    
    Change-Id: I3c03faa28e6cc93f396944579a360d5405c8138e
    CRs-Fixed: 2774789

--- qdf/inc/qdf_types.h
+++ qdf/inc/qdf_types.h
@@ -1311,6 +1311,7 @@ enum qdf_suspend_type {
  * @QDF_VDEV_STOP_RESPONSE_TIMED_OUT: Stop response timeout from FW
  * @QDF_VDEV_DELETE_RESPONSE_TIMED_OUT: Delete response timeout from FW
  * @QDF_VDEV_PEER_DELETE_ALL_RESPONSE_TIMED_OUT: Peer delete all resp timeout
+ * @QDF_HAL_REG_WRITE_FAILURE: HAL register writing failures
  */
 enum qdf_hang_reason {
 	QDF_REASON_UNSPECIFIED,
@@ -1333,6 +1334,7 @@ enum qdf_hang_reason {
 	QDF_VDEV_STOP_RESPONSE_TIMED_OUT,
 	QDF_VDEV_DELETE_RESPONSE_TIMED_OUT,
 	QDF_VDEV_PEER_DELETE_ALL_RESPONSE_TIMED_OUT,
+	QDF_HAL_REG_WRITE_FAILURE,
 };
 
 /**
