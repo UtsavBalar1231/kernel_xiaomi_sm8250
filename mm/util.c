077c073f0b84fe97dbb4ed88c88b9b84a96f3114
Christian König <christian.koenig@amd.com>
2020-09-14 15:09:33 +0200

    mm: introduce vma_set_file function v5
    Add the new vma_set_file() function to allow changing
    vma->vm_file with the necessary refcount dance.
    
    v2: add more users of this.
    v3: add missing EXPORT_SYMBOL, rebase on mmap cleanup,
        add comments why we drop the reference on two occasions.
    v4: make it clear that changing an anonymous vma is illegal.
    v5: move vma_set_file to mm/util.c
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch> (v2)
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Acked-by: Andrew Morton <akpm@linux-foundation.org>
    Link: https://patchwork.freedesktop.org/patch/399360/
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Change-Id: If77ffac122fac2da1ec34b886784381c4648b2c5

--- mm/util.c
+++ mm/util.c
@@ -4,6 +4,7 @@
 #include <linux/compiler.h>
 #include <linux/export.h>
 #include <linux/err.h>
+#include <linux/file.h>
 #include <linux/sched.h>
 #include <linux/sched/mm.h>
 #include <linux/sched/task_stack.h>
@@ -295,6 +296,18 @@ int vma_is_stack_for_current(struct vm_area_struct *vma)
 
 	return (vma->vm_start <= KSTK_ESP(t) && vma->vm_end >= KSTK_ESP(t));
 }
+
+/*
+ * Change backing file, only valid to use during initial VMA setup.
+ */
+void vma_set_file(struct vm_area_struct *vma, struct file *file)
+{
+	/* Changing an anonymous vma with this is illegal */
+	get_file(file);
+	swap(vma->vm_file, file);
+	fput(file);
+}
+EXPORT_SYMBOL(vma_set_file);
 
 #if defined(CONFIG_MMU) && !defined(HAVE_ARCH_PICK_MMAP_LAYOUT)
 void arch_pick_mmap_layout(struct mm_struct *mm, struct rlimit *rlim_stack)
