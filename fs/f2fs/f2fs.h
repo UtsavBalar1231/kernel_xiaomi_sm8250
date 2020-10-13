69ea57565af53e4bc888ea5c65c3b5af718adf3c
a17671 <a17671@motorola.com>
2020-10-13 18:06:28 +0800

    fs:Add Samsung HPB_FS support
    Samsung UFS3.1 driver
    Add the HPB function enhancement
    To F2FS and EXT4 FS
    
    Change-Id: I357efcd64dc0ab9d3b137a4042c98a6580e19623
    Signed-off-by: a17671 <a17671@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1773952
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Qing Chang <qing@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -27,6 +27,10 @@
 #include <linux/fscrypt.h>
 #include <linux/fsverity.h>
 
+#ifdef CONFIG_FS_HPB
+#include <linux/fs_hpb.h>
+#endif
+
 #ifdef CONFIG_F2FS_CHECK_FS
 #define f2fs_bug_on(sbi, condition)	BUG_ON(condition)
 #else
@@ -709,6 +713,9 @@ enum {
 	FI_MMAP_FILE,		/* indicate file was mmapped */
 	FI_ENABLE_COMPRESS,	/* enable compression in "user" compression mode */
 	FI_COMPRESS_RELEASED,	/* compressed blocks were released */
+#ifdef CONFIG_FS_HPB
+	FI_HPB_INODE,		/* HPB */
+#endif
 	FI_MAX,			/* max flag, never be used */
 };
 
