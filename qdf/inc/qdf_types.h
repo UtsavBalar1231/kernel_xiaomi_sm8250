1299022cbf97f9552b199612290c5e9f2af28f60
Arun Kumar Khandavalli <akhandav@codeaurora.org>
2020-10-22 21:38:44 +0530

    qcacmn: Add additional vendor specific hang reason codes
    Add additional hang reason codes in enum qca_wlan_vendor_hang_reason to
    address potential internal failure cases
    
    Change-Id: I4bf154805d3e06241e9fdd799f1e6ffc4c046fca
    CRs-Fixed: 2803747

--- qdf/inc/qdf_types.h
+++ qdf/inc/qdf_types.h
@@ -1311,6 +1311,7 @@ enum qdf_suspend_type {
  * @QDF_VDEV_STOP_RESPONSE_TIMED_OUT: Stop response timeout from FW
  * @QDF_VDEV_DELETE_RESPONSE_TIMED_OUT: Delete response timeout from FW
  * @QDF_VDEV_PEER_DELETE_ALL_RESPONSE_TIMED_OUT: Peer delete all resp timeout
+ * @QDF_WMI_BUF_SEQUENCE_MISMATCH: WMI Tx completion buffer sequence mismatch
  * @QDF_HAL_REG_WRITE_FAILURE: HAL register writing failures
  * @QDF_SUSPEND_NO_CREDIT: host lack of credit after suspend
  */
@@ -1335,6 +1336,7 @@ enum qdf_hang_reason {
 	QDF_VDEV_STOP_RESPONSE_TIMED_OUT,
 	QDF_VDEV_DELETE_RESPONSE_TIMED_OUT,
 	QDF_VDEV_PEER_DELETE_ALL_RESPONSE_TIMED_OUT,
+	QDF_WMI_BUF_SEQUENCE_MISMATCH,
 	QDF_HAL_REG_WRITE_FAILURE,
 	QDF_SUSPEND_NO_CREDIT,
 };
