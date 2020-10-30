3c5297e3a349ecf9d6ad983ab7f928b42ba4e6bb
Jinwei Chen <jinweic@codeaurora.org>
2020-10-30 01:04:30 -0700

    qcacmn: Add reo destination indication info to skb
    Get reo destination indication from REO2SW ring descriptor,
    store it to qdf_nbuf_cb of skb.
    
    Change-Id: I5a4c504b2550229a77a435b1f5ddb78c4abe51aa
    CRs-Fixed: 2809080

--- qdf/inc/i_qdf_nbuf_api_w.h
+++ qdf/inc/i_qdf_nbuf_api_w.h
@@ -120,4 +120,16 @@ static inline uint16_t qdf_nbuf_get_rx_flow_tag(qdf_nbuf_t buf)
 static inline void qdf_nbuf_set_exc_frame(qdf_nbuf_t buf, uint8_t value)
 {
 }
+
+/**
+ * qdf_nbuf_set_rx_reo_dest_ind() - set reo destination indication
+ * @buf: Network buffer
+ * @value: reo destination indication value to set
+ *
+ * Return: none
+ */
+static inline void qdf_nbuf_set_rx_reo_dest_ind(qdf_nbuf_t buf,
+						uint8_t value)
+{
+}
 #endif /* _QDF_NBUF_W_H */
