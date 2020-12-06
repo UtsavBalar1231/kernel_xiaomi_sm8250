99471fdd69ea569964b178820d7e1e10da011e1b
Namjae Jeon <namjae.jeon@samsung.com>
2020-12-06 16:32:57 +0900

    exfat: fix build error with i_blocksize
    Signed-off-by: Namjae Jeon <namjae.jeon@samsung.com>

--- file.c
+++ file.c
@@ -244,7 +244,11 @@ void exfat_truncate(struct inode *inode, loff_t size)
 {
 	struct super_block *sb = inode->i_sb;
 	struct exfat_sb_info *sbi = EXFAT_SB(sb);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 4, 0)
 	unsigned int blocksize = i_blocksize(inode);
+#else
+	unsigned int blocksize = 1 << inode->i_blkbits;
+#endif
 	loff_t aligned_size;
 	int err;
 
