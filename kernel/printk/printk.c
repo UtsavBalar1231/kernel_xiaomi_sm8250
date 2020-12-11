049235a0d7cf0b0627a8a83a5238ba334892d58c
Yaroslav Furman <yaro330@gmail.com>
2020-12-11 19:22:31 +0300

    printk: add userspace messages filter
    Some messages get too annoying.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -831,10 +831,14 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			endp++;
 			len -= endp - line;
 			line = endp;
+			if (strstr(line, "healthd") ||
+				strstr(line, "cacert"))
+				goto free;
 		}
 	}
 
 	printk_emit(facility, level, NULL, 0, "%s", line);
+free:
 	kfree(buf);
 	return ret;
 }
