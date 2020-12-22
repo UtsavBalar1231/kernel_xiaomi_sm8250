b678ff8d96f9f3e890f64a384ba38a5516058b3b
Yu Tian <yutian@codeaurora.org>
2020-12-22 11:00:31 +0800

    qcacld-3.0: Disable TDLS RX pkt statistics in bus vote high
    TDLS pkts statistics API cost 10% CPU cycle of whole RX
    thread. The code logic show it may not be caclulated when
    bus vote larger than medium. But checking routine always
    return false of which may lead more power cost. Fix bus
    vote checking routine to return correct vote status in
    stead of always false value.
    
    Change-Id: I003c9943ef3a4bbc83db6caaf893408e40e4962b
    CRs-Fixed: 2842119

--- core/hdd/src/wlan_hdd_tx_rx.c
+++ core/hdd/src/wlan_hdd_tx_rx.c
@@ -3281,7 +3281,6 @@ void hdd_reset_tcp_adv_win_scale(struct hdd_context *hdd_ctx)
  *
  * Return: True if vote level is high
  */
-#ifdef RX_PERFORMANCE
 bool hdd_is_current_high_throughput(struct hdd_context *hdd_ctx)
 {
 	if (hdd_ctx->cur_vote_level < PLD_BUS_WIDTH_MEDIUM)
@@ -3289,7 +3288,6 @@ bool hdd_is_current_high_throughput(struct hdd_context *hdd_ctx)
 	else
 		return true;
 }
-#endif
 #endif
 
 #ifdef QCA_LL_LEGACY_TX_FLOW_CONTROL
