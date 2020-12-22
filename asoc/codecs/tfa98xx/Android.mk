a28d2421c5dbb4c82e3654b5e4e727518a493495
yundi <v-yundi@xiaomi.com>
2020-12-22 10:10:14 +0800

    AUDIO :fix picasso vangogh monet bb
    ALIOTH-1582
    
    Change-Id: I8c3d60a77fe2f7cfa0a1f97416e9313d42783405
    Signed-off-by: yundi <v-yundi@xiaomi.com>

--- asoc/codecs/tfa98xx/Android.mk
+++ asoc/codecs/tfa98xx/Android.mk
@@ -2,7 +2,7 @@
 
 # Assume no targets will be supported
 # Check if this driver needs be built for current target
-ifeq ($(TARGET_PRODUCT), $(filter $(TARGET_PRODUCT),lmi lmiin lmiinpro))
+ifeq ($(TARGET_PRODUCT), $(filter $(TARGET_PRODUCT),lmi lmiin lmiinpro picasso picassoin monet vangogh))
 $(warning  TARGET_PRODUCT=$(TARGET_PRODUCT))
 ifeq ($(call is-board-platform,kona),true)
 AUDIO_SELECT  := CONFIG_SND_SOC_KONA=m
