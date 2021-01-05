69bd93a77c03fa5224a43395b415e9ba0d825687
Srinivas Dasari <dasaris@codeaurora.org>
2021-01-06 00:39:43 +0530

    qcacld-3.0: Enable/disable roaming based on active NDP sessions
    Currently, active NDP sessions are tracked on the platforms
    where NAN concurrency handling is supported by host through policy
    mgr. Roaming is disabled when an active NDP session is present and
    re-enabled when the last NDP session is teardown. But this tracking
    is not supported on legacy platforms(which don't advertise either
    WMI_SERVICE_NDI_DBS_SUPPORT or WMI_SERVICE_NAN_DISABLE_SUPPORT) as
    policy mgr based tracking is not available.
    So, disable roaming when the first NDP connection happens and
    re-enable when the last peer is departed from HDD APIs in such
    legacy platforms.
    
    Change-Id: I6309af12eab9222dc34a79566f7830b7763826de
    CRs-Fixed: 2846541

--- core/hdd/src/wlan_hdd_nan_datapath.c
+++ core/hdd/src/wlan_hdd_nan_datapath.c
@@ -918,6 +918,12 @@ int hdd_ndp_new_peer_handler(uint8_t vdev_id, uint16_t sta_id,
 					adapter,
 					WLAN_START_ALL_NETIF_QUEUE_N_CARRIER,
 					WLAN_CONTROL_PATH);
+		/*
+		 * This is called only for first peer. So, no.of NDP sessions
+		 * are always 1
+		 */
+		if (!ucfg_is_ndi_dbs_supported(hdd_ctx->psoc))
+			hdd_indicate_active_ndp_cnt(hdd_ctx->psoc, vdev_id, 1);
 	}
 	qdf_mem_free(roam_info);
 	return 0;
@@ -992,5 +998,11 @@ void hdd_ndp_peer_departed_handler(uint8_t vdev_id, uint16_t sta_id,
 		hdd_debug("No more ndp peers.");
 		hdd_cleanup_ndi(hdd_ctx, adapter);
 		qdf_event_set(&adapter->peer_cleanup_done);
+		/*
+		 * This is called only for last peer. So, no.of NDP sessions
+		 * are always 0
+		 */
+		if (!ucfg_is_ndi_dbs_supported(hdd_ctx->psoc))
+			hdd_indicate_active_ndp_cnt(hdd_ctx->psoc, vdev_id, 0);
 	}
 }
