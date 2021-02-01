b97d14033dca0ea169e991d3327c05f1379965a4
Yeshwanth Sriram Guntuka <ysriramg@codeaurora.org>
2021-02-01 22:14:14 +0530

    qcacld-3.0: Modify check to ensure consecutive PN for frags
    Modify check to ensure packet number is consecutive for
    fragments and drop the fragments if the check fails.
    
    Change-Id: Ica24f65aff65ca58bb010c876f27964b5b2bae6a
    CRs-Fixed: 2860242

--- core/dp/txrx/ol_txrx_types.h
+++ core/dp/txrx/ol_txrx_types.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2020 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2021 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -834,7 +834,7 @@ struct ol_txrx_pdev_t {
 	struct {
 		int (*cmp)(union htt_rx_pn_t *new,
 			   union htt_rx_pn_t *old,
-			   int is_unicast, int opmode);
+			   int is_unicast, int opmode, bool strict_chk);
 		int len;
 	} rx_pn[htt_num_sec_types];
 
