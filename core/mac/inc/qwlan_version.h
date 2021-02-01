17c1162d9f34594ad22568ab71a2bbd7e7b2dbea
snandini <snandini@codeaurora.org>
2021-01-31 16:48:49 -0800

    Release 5.2.022.6L
    Release 5.2.022.6L
    
    Change-Id: I9e7a92fcee4c36a1d1506758517daa8a209d1b2a
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            022
-#define QWLAN_VERSION_EXTRA            "K"
+#define QWLAN_VERSION_EXTRA            "L"
 #define QWLAN_VERSION_BUILD            6
 
-#define QWLAN_VERSIONSTR               "5.2.022.6K"
+#define QWLAN_VERSIONSTR               "5.2.022.6L"
 
 #endif /* QWLAN_VERSION_H */
