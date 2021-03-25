d6d5d4637f3017f7113c97bc4222142d7bc3b072
Dyneteve <dyneteve@hentaios.com>
2021-03-25 07:17:50 +0100

    input: touchscreen: xiaomi: Prevent unnecessary input sync
    * Disable the IRQ boost for touch as this only causes garbage data to be spammed & disable accidental touches as this doesn't work properly on AOSP & takes false positives causing alot of issues like lack of inertia in chrome scrolling & gestures not working correctly on launcher
    
    Change-Id: I938e745cabdf44631a245345a270f78fe3d14cb5
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/input/touchscreen/nt36672c/nt36xxx.c
+++ drivers/input/touchscreen/nt36672c/nt36xxx.c
@@ -96,7 +96,9 @@ static int32_t nvt_ts_resume(struct device *dev);
 extern int dsi_panel_lockdown_info_read(unsigned char *plockdowninfo);
 extern void dsi_panel_doubleclick_enable(bool on);
 static int32_t nvt_check_palm(uint8_t input_id, uint8_t *data);
+#if XIAOMI_ROI
 extern void xiaomi_touch_send_btn_tap_key(int status);
+#endif
 uint32_t ENG_RST_ADDR  = 0x7FFF80;
 uint32_t SWRST_N8_ADDR = 0; /* read from dtsi */
 uint32_t SPI_RD_FAST_ADDR = 0;	/* read from dtsi */
--- drivers/input/touchscreen/xiaomi/xiaomi_touch.c
+++ drivers/input/touchscreen/xiaomi/xiaomi_touch.c
@@ -500,6 +500,7 @@ static int xiaomi_touch_parse_dt(struct device *dev, struct xiaomi_touch_pdata *
 	return 0;
 }
 
+#if XIAOMI_ROI
 void xiaomi_touch_send_btn_tap_key(int status)
 {
 	if (xiaomi_touch_dev.key_input_dev) {
@@ -513,6 +514,7 @@ void xiaomi_touch_send_btn_tap_key(int status)
 	}
 }
 EXPORT_SYMBOL(xiaomi_touch_send_btn_tap_key);
+#endif
 
 static int xiaomi_touch_probe(struct platform_device *pdev)
 {
