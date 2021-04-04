b87faebebc524c98ea6e20b7f8a6ca64f4a65e53
LibXZR <xzr467706992@163.com>
2021-04-04 14:18:55 +0800

    drivers: ufs: Disable build-in UFS TW
    * We have our own edits for it. Disable the caf one to
    avoid any accidental situation.
    
    Signed-off-by: LibXZR <xzr467706992@163.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -8883,11 +8883,7 @@ static int ufs_get_device_desc(struct ufs_hba *hba,
 
 
 	/* Enable WB only for UFS-3.1 or UFS-2.2 OR if desc len >= 0x59 */
-	if ((dev_desc->wspecversion >= 0x310) ||
-	    (dev_desc->wspecversion == 0x220) ||
-	    (dev_desc->wmanufacturerid == UFS_VENDOR_TOSHIBA &&
-	     dev_desc->wspecversion >= 0x300 &&
-	     hba->desc_size.dev_desc >= 0x59)) {
+	if (0) {
 		hba->dev_info.d_ext_ufs_feature_sup =
 			desc_buf[DEVICE_DESC_PARAM_EXT_UFS_FEATURE_SUP]
 								<< 24 |
