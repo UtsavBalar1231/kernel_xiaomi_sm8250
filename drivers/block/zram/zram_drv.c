35a97bc40cb92f399ca6387bb171c565c13a9f2c
Park Ju Hyung <qkrwngud825@gmail.com>
2021-04-04 23:51:19 +0900

    zram: trim the backing device upon registration
    We potentially suffer from insufficient free blocks from the SSD,
    causing slowdowns from high GC overheads as we never trim the backing device.
    
    This is not an issue in practice because the userdata partition will be
    trimmed well.
    
    Still, let's do a whole partition discard upon registration for minor
    optimization. It takes less than 60ms to trim a 4GB partition in modern
    asynchronous trim designs used in UFS FTL.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Change-Id: Ib54b5142831cc8c397ff7ba5bc94c23058d80f98
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -380,7 +380,7 @@ static ssize_t backing_dev_store(struct device *dev,
 	struct inode *inode;
 	struct address_space *mapping;
 	unsigned int bitmap_sz;
-	unsigned long nr_pages, *bitmap = NULL;
+	unsigned long nr_pages, nr_size, *bitmap = NULL;
 	struct block_device *bdev = NULL;
 	int err;
 	struct zram *zram = dev_to_zram(dev);
@@ -426,13 +426,20 @@ static ssize_t backing_dev_store(struct device *dev,
 		goto out;
 	}
 
-	nr_pages = i_size_read(inode) >> PAGE_SHIFT;
+	nr_size = i_size_read(inode);
+	nr_pages = nr_size >> PAGE_SHIFT;
 	bitmap_sz = BITS_TO_LONGS(nr_pages) * sizeof(long);
 	bitmap = kvzalloc(bitmap_sz, GFP_KERNEL);
 	if (!bitmap) {
 		err = -ENOMEM;
 		goto out;
 	}
+
+	// Trim the device
+	pr_info("discarding backing device\n");
+	err = blkdev_issue_discard(bdev, 0, nr_size >> 9, GFP_KERNEL, 0);
+	if (err)
+		pr_warn("failed to discard device: %d\n", err);
 
 	reset_bdev(zram);
 
