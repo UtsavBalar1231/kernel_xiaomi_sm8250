61bc85ecb0691cd48ec64608afa392ed3a932567
Park Ju Hyung <qkrwngud825@gmail.com>
2021-04-04 16:47:36 +0900

    zram: enable dedup by default
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Change-Id: I04cc2a0685661ca04bed542a634b19fcb50cd3f4
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -2020,6 +2020,9 @@ static int zram_add(void)
 #ifdef CONFIG_ZRAM_WRITEBACK
 	spin_lock_init(&zram->wb_limit_lock);
 #endif
+#ifdef CONFIG_ZRAM_DEDUP
+	zram->use_dedup = true;
+#endif
 	queue = blk_alloc_queue(GFP_KERNEL);
 	if (!queue) {
 		pr_err("Error allocating disk queue for device %d\n",
