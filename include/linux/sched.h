413952dbb21761aef51a00b256a900953f8acadd
Will McVicker <willmcvicker@google.com>
2021-04-28 10:26:39 -0700

    ANDROID: GKI: sched: fix CRC issues related to CONFIG_SCHED_TUNE
    Move the new stune_idx to use one of the KABI padding fields.
    
    Bug: 185091725
    Fixes: bca62a0ae565 ("sched/tune: Fix improper accounting of tasks")
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Change-Id: Ibd31dfc2158a766f1dcb8e63ddc4193dac6d6077

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -897,9 +897,6 @@ struct task_struct {
 #ifdef CONFIG_CGROUP_SCHED
 	struct task_group		*sched_task_group;
 #endif
-#ifdef CONFIG_SCHED_TUNE
-	int				stune_idx;
-#endif
 	struct sched_dl_entity		dl;
 
 #ifdef CONFIG_UCLAMP_TASK
@@ -1513,7 +1510,12 @@ struct task_struct {
 	struct mutex			futex_exit_mutex;
 #endif
 
+	/* bca62a0ae565 ("sched/tune: Fix improper accounting of tasks") */
+#ifdef CONFIG_SCHED_TUNE
+	ANDROID_KABI_USE(7, int stune_idx);
+#else
 	ANDROID_KABI_RESERVE(7);
+#endif
 	ANDROID_KABI_RESERVE(8);
 
 	/*
