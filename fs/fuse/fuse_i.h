799633f15a3b5860534966080e0d412011aaa16d
Alessio Balsini <balsini@google.com>
2021-05-13 11:58:16 +0100

    ANDROID: fuse/passthrough: API V2 with __u32 open argument
    The initial FUSE passthrough interface has the issue of introducing an
    ioctl which receives as a parameter a data structure containing a
    pointer. What happens is that, depending on the architecture, the size
    of this struct might change, and especially for 32-bit userspace running
    on 64-bit kernel, the size mismatch results into different a single
    ioctl the behavior of which depends on the data that is passed (e.g.,
    with an enum). This is just a poor ioctl design as mentioned by Arnd
    Bergmann [1].
    
    Introduce the new FUSE_PASSTHROUGH_OPEN ioctl which only gets the fd of
    the lower file system, which is a fixed-size __u32, dropping the
    confusing fuse_passthrough_out data structure.
    
    [1] https://lore.kernel.org/lkml/CAK8P3a2K2FzPvqBYL9W=Yut58SFXyetXwU4Fz50G5O3TsS0pPQ@mail.gmail.com/
    
    Bug: 175195837
    Signed-off-by: Alessio Balsini <balsini@google.com>
    Change-Id: I486d71cbe20f3c0c87544fa75da4e2704fe57c7c
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -1015,8 +1015,7 @@ unsigned int fuse_len_args(unsigned int numargs, struct fuse_arg *args);
 u64 fuse_get_unique(struct fuse_iqueue *fiq);
 
 /* passthrough.c */
-int fuse_passthrough_open(struct fuse_dev *fud,
-			  struct fuse_passthrough_out *pto);
+int fuse_passthrough_open(struct fuse_dev *fud, u32 lower_fd);
 int fuse_passthrough_setup(struct fuse_conn *fc, struct fuse_file *ff,
 			   struct fuse_open_out *openarg);
 void fuse_passthrough_release(struct fuse_passthrough *passthrough);
