37372686199f125c3fa40a6b2d2dba7a2f47673f
LibXZR <xzr467706992@163.com>
2021-05-23 23:33:22 +0800

    subsystem_restart: Always performs soft resets when subsystems crash
    * Oneplus is using a dirty list and userspace nodes to override restart_level
    for some subsystems. Why not just ignore all RESET_SOC and use a soft reset
    instead? Nobody wanna to see their device goes randomly reboot sometimes.
    
    * Note that commit 578bed2 is also needed to fix broken irq free when soft
    reset the modem.
    
    Signed-off-by: LibXZR <xzr467706992@163.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/soc/qcom/subsystem_restart.c
+++ drivers/soc/qcom/subsystem_restart.c
@@ -1245,19 +1245,8 @@ int subsystem_restart_dev(struct subsys_device *dev)
 		return 0;
 	}
 
-	switch (dev->restart_level) {
+	__subsystem_restart_dev(dev);
 
-	case RESET_SUBSYS_COUPLED:
-		__subsystem_restart_dev(dev);
-		break;
-	case RESET_SOC:
-		__pm_stay_awake(dev->ssr_wlock);
-		schedule_work(&dev->device_restart_work);
-		return 0;
-	default:
-		panic("subsys-restart: Unknown restart level!\n");
-		break;
-	}
 	module_put(dev->owner);
 	put_device(&dev->dev);
 
