ea1ce365d682ea7c2c2a543d88e60d1b8a18a06f
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-05-31 10:03:42 +0200

    net: cnss2: Add support for loading different bdwlan firmwares
    Change-Id: Ic9d52c5a82b08b61811918af4436e179d4917bc4
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/net/wireless/cnss2/qmi.c
+++ drivers/net/wireless/cnss2/qmi.c
@@ -4,6 +4,7 @@
 #include <linux/firmware.h>
 #include <linux/module.h>
 #include <linux/soc/qcom/qmi.h>
+#include <soc/qcom/socinfo.h>
 
 #include "bus.h"
 #include "debug.h"
@@ -14,7 +15,33 @@
 #define WLFW_CLIENT_ID			0x4b4e454c
 #define BDF_FILE_NAME_PREFIX		"bdwlan"
 #define ELF_BDF_FILE_NAME		"bdwlan.elf"
+
+#define ELF_BDF_FILE_NAME_J11		"bd_j11.elf"
+#define ELF_BDF_FILE_NAME_J11_B_BOM		"bd_j11_b.elf"
+#define ELF_BDF_FILE_NAME_J11_INDIA		"bd_j11in.elf"
+#define ELF_BDF_FILE_NAME_J11_GLOBAL		"bd_j11gl.elf"
+
+#define ELF_BDF_FILE_NAME_GLOBAL	 "bd_j1gl.elf"
+#define ELF_BDF_FILE_NAME_INDIA		 "bd_j1in.elf"
+#define ELF_BDF_FILE_NAME_B_BOM		 "bd_j1_b.elf"
+
+#define ELF_BDF_FILE_NAME_J1S		 "bd_j1s.elf"
+
+#define ELF_BDF_FILE_NAME_J2S		 "bd_j2s.elf"
+
+#define ELF_BDF_FILE_NAME_J3S		 "bd_j3s.elf"
+#define ELF_BDF_FILE_NAME_J3S_GLOBAL	 "bd_j3sgl.elf"
+#define ELF_BDF_FILE_NAME_J3S_INDIA	 "bd_j3sin.elf"
+
+#define ELF_BDF_FILE_NAME_K11A		 "bd_k11a.elf"
+#define ELF_BDF_FILE_NAME_K11A_GLOBAL	 "bd_k11agl.elf"
+#define ELF_BDF_FILE_NAME_K11A_INDIA	 "bd_k11ain.elf"
+
+#define ELF_BDF_FILE_NAME_K81            "bd_k81.elf"
+#define ELF_BDF_FILE_NAME_K81A           "bd_k81a.elf"
+
 #define ELF_BDF_FILE_NAME_GF		"bdwlang.elf"
+
 #define ELF_BDF_FILE_NAME_PREFIX	"bdwlan.e"
 #define ELF_BDF_FILE_NAME_GF_PREFIX	"bdwlang.e"
 #define BIN_BDF_FILE_NAME		"bdwlan.bin"
@@ -22,6 +49,7 @@
 #define BIN_BDF_FILE_NAME_PREFIX	"bdwlan.b"
 #define BIN_BDF_FILE_NAME_GF_PREFIX	"bdwlang.b"
 #define REGDB_FILE_NAME			"regdb.bin"
+#define REGDB_FILE_NAME_J11		"regdb_j11.bin"
 #define DUMMY_BDF_FILE_NAME		"bdwlan.dmy"
 #define CHIP_ID_GF_MASK			0x10
 
@@ -506,17 +534,70 @@ static int cnss_get_bdf_file_name(struct cnss_plat_data *plat_priv,
 {
 	char filename_tmp[MAX_FIRMWARE_NAME_LEN];
 	int ret = 0;
+	int hw_platform_ver = -1;
+	uint32_t hw_country_ver = 0;
 
+	hw_platform_ver = get_hw_version_platform();
+	hw_country_ver = get_hw_country_version();
+
+        cnss_pr_dbg("hw_platform_ver is %d\n", hw_platform_ver);
 	switch (bdf_type) {
 	case CNSS_BDF_ELF:
 		/* Board ID will be equal or less than 0xFF in GF mask case */
 		if (plat_priv->board_info.board_id == 0xFF) {
-			if (plat_priv->chip_info.chip_id & CHIP_ID_GF_MASK)
-				snprintf(filename_tmp, filename_len,
-					 ELF_BDF_FILE_NAME_GF);
-			else
-				snprintf(filename_tmp, filename_len,
-					 ELF_BDF_FILE_NAME);
+			if (hw_platform_ver == HARDWARE_PLATFORM_LMI) {
+				if (get_hw_country_version() == (uint32_t)CountryGlobal)
+				    snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_J11_GLOBAL);
+				else if (get_hw_country_version() == (uint32_t)CountryIndia)
+				    snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_J11_INDIA);
+				else {
+					if ((get_hw_version_minor() == (uint32_t)HW_MINOR_VERSION_B) &&
+					    (get_hw_version_major() == (uint32_t)HW_MAJOR_VERSION_B))
+						snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_J11_B_BOM);
+					else
+						snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_J11);
+				}
+			} else if (hw_platform_ver == HARDWARE_PLATFORM_CAS) {
+				snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_J1S);
+			} else if (hw_platform_ver == HARDWARE_PLATFORM_THYME) {
+				snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_J2S);
+			} else if (hw_platform_ver == HARDWARE_PLATFORM_APOLLO) {
+				if (get_hw_country_version() == (uint32_t)CountryGlobal)
+				    snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_J3S_GLOBAL);
+				else if (get_hw_country_version() == (uint32_t)CountryIndia)
+				    snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_J3S_INDIA);
+				else
+				    snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_J3S);
+			} else if (hw_platform_ver == HARDWARE_PLATFORM_ALIOTH) {
+				if (get_hw_country_version() == (uint32_t)CountryGlobal)
+				    snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_K11A_GLOBAL);
+				else if (get_hw_country_version() == (uint32_t)CountryIndia)
+				    snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_K11A_INDIA);
+				else
+				    snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_K11A);
+			} else if (hw_platform_ver == HARDWARE_PLATFORM_ENUMA) {
+				snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_K81);
+			} else if (hw_platform_ver == HARDWARE_PLATFORM_ELISH) {
+				snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_K81A);
+			} else {
+				if (hw_country_ver == (uint32_t)CountryGlobal)
+					snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_GLOBAL);
+				else if (hw_country_ver == (uint32_t)CountryIndia)
+					snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_INDIA);
+				else {
+					if ((get_hw_version_minor() == (uint32_t)HW_MINOR_VERSION_B) &&
+					    (get_hw_version_major() == (uint32_t)HW_MAJOR_VERSION_B))
+						snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_B_BOM);
+					else {
+						if (plat_priv->chip_info.chip_id & CHIP_ID_GF_MASK)
+							snprintf(filename_tmp, filename_len,
+								 ELF_BDF_FILE_NAME_GF);
+						else
+							snprintf(filename_tmp, filename_len,
+								 ELF_BDF_FILE_NAME);
+					}
+				}
+			}
 		} else if (plat_priv->board_info.board_id < 0xFF) {
 			if (plat_priv->chip_info.chip_id & CHIP_ID_GF_MASK)
 				snprintf(filename_tmp, filename_len,
@@ -558,7 +639,10 @@ static int cnss_get_bdf_file_name(struct cnss_plat_data *plat_priv,
 		}
 		break;
 	case CNSS_BDF_REGDB:
-		snprintf(filename_tmp, filename_len, REGDB_FILE_NAME);
+		if (hw_platform_ver == HARDWARE_PLATFORM_LMI)
+			snprintf(filename_tmp, filename_len, REGDB_FILE_NAME_J11);
+		else
+			snprintf(filename_tmp, filename_len, REGDB_FILE_NAME);
 		break;
 	case CNSS_BDF_DUMMY:
 		cnss_pr_dbg("CNSS_BDF_DUMMY is set, sending dummy BDF\n");
