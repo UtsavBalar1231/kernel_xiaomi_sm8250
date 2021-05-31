4712f61987b4bf3546638bb24e03a28c3b0e9887
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-05-31 18:21:19 +0200

    staging: android: ion: Add xiaomi ion camera heap modifications
    Change-Id: I8d188c8209e2cd614c552f6a7def95114a806895
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/staging/android/ion/ion_camera_heap.h
+++ drivers/staging/android/ion/ion_camera_heap.h
@@ -1,22 +1,29 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2017-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2019. Xiaomi Technology Co.
  * Copyright (C) 2021 XiaoMi, Inc.
+ *
+ * All Rights Reserved.
+ *
+ * Confidential and Proprietary - Xiaomi Technology Co.
  */
+
 #include <soc/qcom/secure_buffer.h>
 #include "ion.h"
 
-#ifndef _ION_SYSTEM_HEAP_H
-#define _ION_SYSTEM_HEAP_H
+#ifndef _ION_CAMERA_HEAP_H
+#define _ION_CAMERA_HEAP_H
 
 #ifndef CONFIG_ALLOC_BUFFERS_IN_4K_CHUNKS
 #if defined(CONFIG_IOMMU_IO_PGTABLE_ARMV7S)
 static const unsigned int orders[] = {8, 4, 0};
+static const unsigned int cam_reserved_counts[] = {1024, 2048, 3096};
 #else
-static const unsigned int orders[] = {9, 4, 0};
+static const unsigned int orders[] = {8, 4, 0};
+static const unsigned int cam_reserved_counts[] = {20, 1600, 35840};
 #endif
 #else
 static const unsigned int orders[] = {0};
+static const unsigned int cam_reserved_counts[] = {0};
 #endif
 
 #define NUM_ORDERS ARRAY_SIZE(orders)
@@ -29,13 +36,14 @@ enum ion_kthread_type {
 	ION_MAX_NUM_KTHREADS
 };
 
-struct ion_system_heap {
+struct ion_camera_heap {
 	struct ion_heap heap;
 	struct ion_page_pool *uncached_pools[MAX_ORDER];
 	struct ion_page_pool *cached_pools[MAX_ORDER];
 	/* worker threads to refill the pool */
 	struct task_struct *kworker[ION_MAX_NUM_KTHREADS];
 	struct ion_page_pool *secure_pools[VMID_LAST][MAX_ORDER];
+	struct ion_page_pool *reserved_pools[MAX_ORDER];
 	/* Prevents unnecessary page splitting */
 	struct mutex split_page_mutex;
 };
@@ -47,10 +55,14 @@ struct page_info {
 	struct list_head list;
 };
 
-int order_to_index(unsigned int order);
+int camera_camera_order_to_index(unsigned int order);
 
-void free_buffer_page(struct ion_system_heap *heap,
+bool reserved_pools_full(struct ion_camera_heap *heap,unsigned int order);
+
+int reserved_pool_page_count(struct ion_camera_heap *heap, unsigned int order);
+
+void free_camera_buffer_page(struct ion_camera_heap *heap,
 		      struct ion_buffer *buffer, struct page *page,
 		      unsigned int order);
 
-#endif /* _ION_SYSTEM_HEAP_H */
+#endif /* _ION_CAMERA_HEAP_H */
