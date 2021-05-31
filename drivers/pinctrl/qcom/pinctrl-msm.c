c2ce009c37de490a90b65af37147c2f23ed02a10
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-05-31 10:35:19 +0200

    pinctrl: msm: Disable access to specified reserved gpios
    Change-Id: Ia960cd37ad4b9998ec8a2d04e1067033420591ee
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/pinctrl/qcom/pinctrl-msm.c
+++ drivers/pinctrl/qcom/pinctrl-msm.c
@@ -571,8 +571,16 @@ static void msm_gpio_dbg_show(struct seq_file *s, struct gpio_chip *chip)
 	unsigned gpio = chip->base;
 	unsigned i;
 
-	for (i = 0; i < chip->ngpio; i++, gpio++)
+	for (i = 0; i < chip->ngpio; i++, gpio++) {
+		/**
+		 * bypass NFC SPI GPIO: 28-31 is NFC SE SPI
+		 * bypass FP SPI GPIO: 40-43 is FP SPI
+		 */
+		if ((i >= 28 && i <= 31) || (i >= 40 && i <= 43)) {
+			continue;
+		}
 		msm_gpio_dbg_show_one(s, NULL, chip, i, gpio);
+	}
 }
 
 #else
