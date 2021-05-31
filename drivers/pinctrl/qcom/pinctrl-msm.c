6ab0aa91a39d29600bd6f0d781ba7c9014a0ab31
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-05-31 06:03:45 +0200

    drivers: gpio: Add support to dump GPIOs debug logs
    Change-Id: Ifc952cecc97fdf433edabd2d6017812762359078
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/pinctrl/qcom/pinctrl-msm.c
+++ drivers/pinctrl/qcom/pinctrl-msm.c
@@ -510,6 +510,15 @@ static void msm_gpio_set(struct gpio_chip *chip, unsigned offset, int value)
 
 #ifdef CONFIG_DEBUG_FS
 #include <linux/seq_file.h>
+#define msm_gpio_debug_output(m, c, fmt, ...)		\
+do {							\
+	if (m)						\
+		seq_printf(m, fmt, ##__VA_ARGS__);	\
+	else if (c)					\
+		pr_cont(fmt, ##__VA_ARGS__);		\
+	else						\
+		pr_info(fmt, ##__VA_ARGS__);		\
+} while (0)
 
 static void msm_gpio_dbg_show_one(struct seq_file *s,
 				  struct pinctrl_dev *pctldev,
@@ -556,14 +565,14 @@ static void msm_gpio_dbg_show_one(struct seq_file *s,
 	else
 		val = !!(io_reg & BIT(g->in_bit));
 
-	seq_printf(s, " %-8s: %-3s", g->name, is_out ? "out" : "in");
-	seq_printf(s, " %-4s func%d", val ? "high" : "low", func);
-	seq_printf(s, " %dmA", msm_regval_to_drive(drive));
+	msm_gpio_debug_output(s, 1, " %-8s: %-3s %d" , g->name, is_out ? "out" : "in" , func);
+	msm_gpio_debug_output(s, 1, " %s", val ? "high":"low");
+	msm_gpio_debug_output(s, 1, " %dmA", msm_regval_to_drive(drive));
 	if (pctrl->soc->pull_no_keeper)
-		seq_printf(s, " %s", pulls_no_keeper[pull]);
+		msm_gpio_debug_output(s, 1, " %s", pulls_no_keeper[pull]);
 	else
-		seq_printf(s, " %s", pulls_keeper[pull]);
-	seq_puts(s, "\n");
+		msm_gpio_debug_output(s, 1,  " %s", pulls_keeper[pull]);
+	msm_gpio_debug_output(s, 1, "\n");
 }
 
 static void msm_gpio_dbg_show(struct seq_file *s, struct gpio_chip *chip)
@@ -573,6 +582,7 @@ static void msm_gpio_dbg_show(struct seq_file *s, struct gpio_chip *chip)
 
 	for (i = 0; i < chip->ngpio; i++, gpio++)
 		msm_gpio_dbg_show_one(s, NULL, chip, i, gpio);
+		msm_gpio_debug_output(s, 1, "\n");
 }
 
 #else
