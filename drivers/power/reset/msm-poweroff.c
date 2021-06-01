c07567a14fc194e315e76fb8ec6e0c55f66aa307
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-06-01 09:49:50 +0200

    ARM64: Add support to log device powerup reason
    Change-Id: Ia179268d95b8b377dce6413a2d5b9700d972f625
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -38,7 +38,11 @@
 #define SCM_DLOAD_FULLDUMP		0X10
 #define SCM_EDLOAD_MODE			0X01
 #define SCM_DLOAD_CMD			0x10
+#ifndef CONFIG_BOARD_XIAOMI
 #define SCM_DLOAD_MINIDUMP		0X20
+#else
+#define SCM_DLOAD_MINIDUMP		0X40
+#endif
 #define SCM_DLOAD_BOTHDUMPS	(SCM_DLOAD_MINIDUMP | SCM_DLOAD_FULLDUMP)
 
 #define DL_MODE_PROP "qcom,msm-imem-download_mode"
@@ -65,8 +69,13 @@ static void scm_disable_sdi(void);
 static int download_mode = 1;
 static struct kobject dload_kobj;
 
+#ifndef CONFIG_BOARD_XIAOMI
 static int in_panic;
 static int dload_type = SCM_DLOAD_FULLDUMP;
+#else
+int in_panic = 0;
+static int dload_type = SCM_DLOAD_BOTHDUMPS;
+#endif
 static void *dload_mode_addr;
 static bool dload_mode_enabled;
 static void *emergency_dload_mode_addr;
@@ -495,7 +504,9 @@ static void msm_restart_prepare(const char *cmd)
 	else
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_HARD_RESET);
 
-	if (cmd != NULL) {
+	if (in_panic) {
+		qpnp_pon_set_restart_reason(PON_RESTART_REASON_PANIC);
+	} else if (cmd != NULL) {
 		if (!strncmp(cmd, "bootloader", 10)) {
 			qpnp_pon_set_restart_reason(
 				PON_RESTART_REASON_BOOTLOADER);
@@ -503,6 +514,10 @@ static void msm_restart_prepare(const char *cmd)
 		} else if (!strncmp(cmd, "recovery", 8)) {
 			qpnp_pon_set_restart_reason(
 				PON_RESTART_REASON_RECOVERY);
+			__raw_writel(0x77665502, restart_reason);
+		} else if (!strncmp(cmd, "exaid", 5)) {
+			qpnp_pon_set_restart_reason(
+				PON_RESTART_REASON_EXAID);
 			__raw_writel(0x77665502, restart_reason);
 		} else if (!strcmp(cmd, "rtc")) {
 			qpnp_pon_set_restart_reason(
@@ -529,10 +544,15 @@ static void msm_restart_prepare(const char *cmd)
 				__raw_writel(0x6f656d00 | (code & 0xff),
 					     restart_reason);
 		} else if (!strncmp(cmd, "edl", 3)) {
+			if (0)
 			enable_emergency_dload_mode();
 		} else {
+			qpnp_pon_set_restart_reason(PON_RESTART_REASON_NORMAL);
 			__raw_writel(0x77665501, restart_reason);
 		}
+	} else {
+		qpnp_pon_set_restart_reason(PON_RESTART_REASON_NORMAL);
+		__raw_writel(0x77665501, restart_reason);
 	}
 
 	flush_cache_all();
--- include/linux/of_fdt.h
+++ include/linux/of_fdt.h
@@ -44,6 +44,10 @@ extern void *initial_boot_params;
 extern char __dtb_start[];
 extern char __dtb_end[];
 
+#if IS_ENABLED(CONFIG_OF_FLATTREE) && IS_ENABLED(CONFIG_BOOT_INFO)
+extern void early_init_dt_setup_pureason_arch(unsigned long pu_reason);
+#endif
+
 /* Other Prototypes */
 extern u64 of_flat_dt_translate_address(unsigned long node);
 extern void of_fdt_limit_memory(int limit);
