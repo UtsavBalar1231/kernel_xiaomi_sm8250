08ca00771aa06b9a0fae18da5d900167246165bf
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-06-01 09:00:47 +0200

    debug: qcom: rpmh: Print rpmh statistics in dmesg
    Change-Id: I58de903b05dad6a575d70ad8220176a8cd174443
    
    g
    
    Change-Id: I134cfd657d19f8f9620481a94b205445f59f0b24
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/soc/qcom/rpm_stats.c
+++ drivers/soc/qcom/rpm_stats.c
@@ -15,6 +15,7 @@
 #include <linux/of.h>
 #include <linux/uaccess.h>
 #include <asm/arch_timer.h>
+static void __iomem *reg_base;
 
 #define RPM_STATS_NUM_REC	2
 #define MSM_ARCH_TIMER_FREQ	19200000
@@ -167,6 +168,26 @@ static inline int msm_rpmstats_copy_stats(
 	return length;
 }
 
+void rpmh_status_print_enabled(void)
+{
+	int i;
+	struct msm_rpm_stats_data data;
+	char stat_type[5];
+	for (i = 0; i < 3; i++) {
+		stat_type[4] = 0;
+		data.stat_type = msm_rpmstats_read_long_register(reg_base, i,
+				offsetof(struct msm_rpm_stats_data,
+					stat_type));
+		data.count = msm_rpmstats_read_long_register(reg_base, i,
+				offsetof(struct msm_rpm_stats_data, count));
+		memcpy(stat_type, &data.stat_type, sizeof(u32));
+		pr_info("RPM Mode:%s----count:%d\n",stat_type, data.count);
+	}
+
+}
+
+EXPORT_SYMBOL_GPL(rpmh_status_print_enabled);
+
 static ssize_t rpmstats_show(struct kobject *kobj,
 			struct kobj_attribute *attr, char *buf)
 {
@@ -272,6 +293,8 @@ static int msm_rpmstats_probe(struct platform_device *pdev)
 
 	msm_rpmstats_create_sysfs(pdev, pdata);
 
+	reg_base = ioremap_nocache(pdata->phys_addr_base,
+					pdata->phys_size);
 	return 0;
 }
 
