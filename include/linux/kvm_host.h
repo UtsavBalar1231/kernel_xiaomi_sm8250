ce800041d94bb1e314835153b84772c0659acbd0
Guenter Roeck <groeck@chromium.org>
2021-06-30 11:54:20 -0700

    FIXUP: CHROMIUM: mm: multigenerational lru: scan kvm mmu pages
    0-day reports:
    
    arch/arm64/kvm/../../../virt/kvm/kvm_main.c:571:13: warning:
    	no previous prototype for 'kvm_arch_mmu_clear_young_walk'
    
    BUG=b:123039911
    TEST=Ran crostini.DiskIOPerf and crostini.VimCompile
    
    Change-Id: Ib8ba610fe46a165044d17914537d3aa5b2abf525
    Disallow-Recycled-Builds: test-failures
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2998147
    Tested-by: Yu Zhao <yuzhao@chromium.org>
    Auto-Submit: Yu Zhao <yuzhao@chromium.org>
    Reviewed-by: Yu Zhao <yuzhao@chromium.org>
    Commit-Queue: Yu Zhao <yuzhao@chromium.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -563,6 +563,9 @@ static inline int kvm_vcpu_get_idx(struct kvm_vcpu *vcpu)
 int kvm_vcpu_init(struct kvm_vcpu *vcpu, struct kvm *kvm, unsigned id);
 void kvm_vcpu_uninit(struct kvm_vcpu *vcpu);
 
+void kvm_arch_mmu_clear_young_walk(struct kvm *kvm,
+				   struct mmu_notifier_walk *walk);
+
 void vcpu_load(struct kvm_vcpu *vcpu);
 void vcpu_put(struct kvm_vcpu *vcpu);
 
