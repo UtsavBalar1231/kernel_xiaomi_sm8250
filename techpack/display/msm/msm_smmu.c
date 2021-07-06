aa227d84c188d9382181878cba14096e6f0b15b2
Miguel de Dios <migueldedios@google.com>
2021-07-06 14:30:53 +0900

    techpack: display: Specify sync probe for msmdrm_smmu
    Bug: 115776306
    Bug: 77146523
    
    Signed-off-by: Miguel de Dios <migueldedios@google.com>
    Change-Id: Iff5a580c03e4733116f862ae26e2b67ceafb7e16
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- techpack/display/msm/msm_smmu.c
+++ techpack/display/msm/msm_smmu.c
@@ -497,6 +497,7 @@ static struct platform_driver msm_smmu_driver = {
 		.name = "msmdrm_smmu",
 		.of_match_table = msm_smmu_dt_match,
 		.suppress_bind_attrs = true,
+		.probe_type = PROBE_FORCE_SYNCHRONOUS,
 	},
 };
 
