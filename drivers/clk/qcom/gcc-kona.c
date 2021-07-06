e8d853a6286215a44482083e2a457eda7e47c8e1
Miguel de Dios <migueldedios@google.com>
2021-07-06 14:28:32 +0900

    drivers: clk: Specify sync probe for gcc-kona
    Bug: 115776306
    Bug: 77146523
    
    Signed-off-by: Miguel de Dios <migueldedios@google.com>
    Change-Id: I6c9e74fa5ee738581e84c7116333563af0206e31
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/clk/qcom/gcc-kona.c
+++ drivers/clk/qcom/gcc-kona.c
@@ -4424,6 +4424,7 @@ static struct platform_driver gcc_kona_driver = {
 	.driver = {
 		.name = "gcc-kona",
 		.of_match_table = gcc_kona_match_table,
+		.probe_type = PROBE_FORCE_SYNCHRONOUS,
 	},
 };
 
