31daa90e8be67f7bd9b5740b0171a05b91f4c015
Miguel de Dios <migueldedios@google.com>
2021-07-06 14:32:41 +0900

    drivers: i2c: Specify sync probe for dummy i2c driver
    Bug: 115776306
    Bug: 77146523
    
    Signed-off-by: Miguel de Dios <migueldedios@google.com>
    Change-Id: I86220780fa75feba1712e6032edbdf2c361ee322
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/i2c/i2c-core-base.c
+++ drivers/i2c/i2c-core-base.c
@@ -858,10 +858,13 @@ static int dummy_remove(struct i2c_client *client)
 }
 
 static struct i2c_driver dummy_driver = {
-	.driver.name	= "dummy",
 	.probe		= dummy_probe,
 	.remove		= dummy_remove,
 	.id_table	= dummy_id,
+	.driver = {
+		.name = "dummy",
+		.probe_type = PROBE_FORCE_SYNCHRONOUS,
+	},
 };
 
 /**
