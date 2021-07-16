619ea867167aba110b546d3c84817875f36cb9a3
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-07-16 06:31:30 +0200

    input: touchscreen: focaltech_touch: Disable Production test module
    Change-Id: I12c92f94dab3042c5fc6a14af354e8d5b4e7f950

--- drivers/input/touchscreen/focaltech_touch/focaltech_config.h
+++ drivers/input/touchscreen/focaltech_touch/focaltech_config.h
@@ -130,7 +130,7 @@
  * Production test enable
  * 1: enable, 0:disable(default)
  */
-#define FTS_TEST_EN                             1
+#define FTS_TEST_EN                             0
 
 /*
  * Glove mode enable
