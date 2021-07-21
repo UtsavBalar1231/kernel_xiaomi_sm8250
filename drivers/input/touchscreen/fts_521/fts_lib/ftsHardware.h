c75069c7363791d39ae964b128f3ece2a3746a0f
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-07-21 06:23:39 +0200

    input: touchscreen: fts_521: Speed up SPI clock frequency to 10 Mhz
    Change-Id: I8a191a18851bf313892b57dee8ea45b78d21b48d
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/input/touchscreen/fts_521/fts_lib/ftsHardware.h
+++ drivers/input/touchscreen/fts_521/fts_lib/ftsHardware.h
@@ -39,7 +39,7 @@
 #else
 #define SPI4_WIRE													/*comment if the master is SPI3 wires (MOSI and MISO share same line)*/
 #define SPI_DELAY_CS							10					/*time in usec to wait before rising the CS*/
-#define SPI_CLOCK_FREQ							7000000				/*clock frequency in Hz of the SPI bus*/
+#define SPI_CLOCK_FREQ							10000000				/*clock frequency in Hz of the SPI bus*/
 #endif
 
 #define IER_ENABLE								0x41				/*value to write in IER_ADDR to enable the interrupts*/
