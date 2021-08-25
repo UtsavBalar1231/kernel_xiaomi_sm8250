e3ad45fdefaf26d804d7fcfd9daa2a0bcc0fe49a
Bapiraju Alla <balla@codeaurora.org>
2021-08-25 12:56:48 +0530

    qcacld-3.0: Reset wlan suspended flag after SSR
    When there is an SSR in between suspend resume sequence,
    the resume request will be rejected due to SSR and suspend flag is
    not being reset after SSR. Because of this Tx frames are getting
    dropped at hdd.
    
    To avoid this, reset wlan suspended flag after SSR.
    
    Change-Id: I3e0e9cb207da0933a85810820038b5b6a90310a6
    CRs-Fixed: 3020583

--- core/hdd/src/wlan_hdd_power.c
+++ core/hdd/src/wlan_hdd_power.c
@@ -1628,6 +1628,10 @@ QDF_STATUS hdd_wlan_re_init(void)
 		hdd_ssr_restart_sap(hdd_ctx);
 	hdd_is_interface_down_during_ssr(hdd_ctx);
 	hdd_wlan_ssr_reinit_event();
+
+	if (hdd_ctx->hdd_wlan_suspended)
+		hdd_ctx->hdd_wlan_suspended = false;
+
 	return QDF_STATUS_SUCCESS;
 
 err_re_init:
