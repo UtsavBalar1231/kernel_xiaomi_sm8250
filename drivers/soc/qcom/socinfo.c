405ae67c752b8708998412f972db76e91b76012e
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-09-08 06:28:26 +0530

    soc: qcom: Add support for k81 (enuma)/ k81a (elish) platforms
    Change-Id: I5aa0ffe44bab1cc47a6419fbdfeef62f82b87811
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -62,6 +62,8 @@ enum {
 	HW_PLATFORM_J2  = 43,
 	HW_PLATFORM_K11A = 44,
 	HW_PLATFORM_J2S = 45,
+	HW_PLATFORM_K81 = 46,
+	HW_PLATFORM_K81A = 47,
 	HW_PLATFORM_HDK = 31,
 	HW_PLATFORM_IDP = 34,
 	HW_PLATFORM_INVALID
@@ -91,6 +93,8 @@ const char *hw_platform[] = {
 	[HW_PLATFORM_J3S] = "APOLLO",
 	[HW_PLATFORM_K11A] = "ALIOTH",
 	[HW_PLATFORM_J2S] = "THYME",
+	[HW_PLATFORM_K81] = "ENUMA",
+	[HW_PLATFORM_K81A] = "ELISH",
 	[HW_PLATFORM_HDK] = "HDK",
 	[HW_PLATFORM_IDP] = "IDP"
 };
@@ -1740,6 +1744,10 @@ uint32_t get_hw_version_platform(void)
 		return HARDWARE_PLATFORM_APOLLO;
 	if (hw_type == HW_PLATFORM_K11A)
 		return HARDWARE_PLATFORM_ALIOTH;
+	if (hw_type == HW_PLATFORM_K81)
+		return HARDWARE_PLATFORM_ENUMA;
+	if (hw_type == HW_PLATFORM_K81A)
+		return HARDWARE_PLATFORM_ELISH;
         if (hw_type == HW_PLATFORM_J2S)
                 return HARDWARE_PLATFORM_THYME;
 	else
