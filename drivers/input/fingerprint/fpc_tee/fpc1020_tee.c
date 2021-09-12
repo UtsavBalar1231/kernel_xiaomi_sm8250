7174ec9a780069ff2744a969b2d4df24c85420cb
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-09-12 18:21:07 +0530

    fingerprint: fpc_tee: Fix uninitialized warning in vendor_update
    ../drivers/input/fingerprint/fpc_tee/fpc1020_tee.c: In function 'vendor_update':
    ../drivers/input/fingerprint/fpc_tee/fpc1020_tee.c:752:24: warning: 'rc' is used uninitialized [-Wuninitialized]
      752 |         return rc ? rc : count;
          |                ~~~~~~~~^~~~~~~
    ../drivers/input/fingerprint/fpc_tee/fpc1020_tee.c:749:13: note: 'rc' was declared here
      749 |         int rc;
          |             ^~
    
    Change-Id: I8a2f951c4c6e6c462e063901064af97ce3459a55
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
@@ -746,7 +746,7 @@ static ssize_t vendor_update(struct device *dev,
 			     struct device_attribute *attr,
 			     const char *buf, size_t count)
 {
-	int rc;
+	int rc = 0;
 	//rc = add_hw_component_info(HWMON_CONPONENT_NAME, "ic_vendor", (char *)buf);
 	//rc = add_hw_component_info(HWMON_CONPONENT_NAME, "reserve", "0xFFFF");
 	return rc ? rc : count;
