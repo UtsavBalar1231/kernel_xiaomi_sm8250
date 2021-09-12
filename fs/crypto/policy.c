f6a54658c31a4703e555dd1eaf5f8ecb0f37b5b5
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-09-12 08:02:45 +0200

    crypto: Fix misleading indentation in fscrypt_update_context
    ../fs/crypto/policy.c: In function 'fscrypt_update_context':
    ../fs/crypto/policy.c:653:17: warning: this 'if' clause does not guard... [-Wmisleading-indentation]
      653 |                 if (!strcmp(boot, SDHCI))
          |                 ^~
    ../fs/crypto/policy.c:655:25: note: ...this statement, but the latter is misleadingly indented as if it were guarded by the 'if'
      655 |                         return 0;
          |                         ^~~~~~
    
    Change-Id: If68a63f1c6af78b71f419f0a4b056ec78d197e97
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- fs/crypto/policy.c
+++ fs/crypto/policy.c
@@ -652,7 +652,7 @@ static int fscrypt_update_context(union fscrypt_context *ctx)
 	if (!fscrypt_find_storage_type(&boot)) {
 		if (!strcmp(boot, SDHCI))
 			ctx->v1.flags |= FSCRYPT_POLICY_FLAG_IV_INO_LBLK_32;
-			return 0;
+		return 0;
 	}
 	return -EINVAL;
 }
