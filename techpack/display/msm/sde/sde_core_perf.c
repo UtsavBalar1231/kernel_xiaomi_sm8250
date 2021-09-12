abfc540f50b110bea7971e3f26fd3290f0d46ba5
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-09-12 21:47:28 +0530

    disp: msm: sde: Fix format warning in _sde_core_perf_calc_doze_suspend
    In file included from ../include/linux/printk.h:7,
                     from ../include/linux/kernel.h:13,
                     from ../include/linux/list.h:9,
                     from ../include/linux/wait.h:7,
                     from ../include/linux/wait_bit.h:8,
                     from ../include/linux/fs.h:6,
                     from ../include/linux/debugfs.h:15,
                     from ../techpack/display/msm/sde/sde_core_perf.c:8:
    ../techpack/display/msm/sde/sde_core_perf.c: In function '_sde_core_perf_calc_doze_suspend':
    ../include/linux/kern_levels.h:5:25: warning: format '%d' expects argument of type 'int', but argument 6 has type 'uint64_t' {aka 'long long unsigned int'} [-Wformat=]
        5 | #define KERN_SOH        "\001"          /* ASCII Start Of Header */
          |                         ^~~~~~
    ../include/linux/kern_levels.h:11:25: note: in expansion of macro 'KERN_SOH'
       11 | #define KERN_ERR        KERN_SOH "3"    /* error conditions */
          |                         ^~~~~~~~
    ../include/linux/printk.h:304:16: note: in expansion of macro 'KERN_ERR'
      304 |         printk(KERN_ERR pr_fmt(fmt), ##__VA_ARGS__)
          |                ^~~~~~~~
    ../techpack/display/msm/sde/sde_kms.h:82:29: note: in expansion of macro 'pr_err'
       82 | #define SDE_ERROR(fmt, ...) pr_err("[sde error]" fmt, ##__VA_ARGS__)
          |                             ^~~~~~
    ../techpack/display/msm/sde/sde_core_perf.c:140:25: note: in expansion of macro 'SDE_ERROR'
      140 |                         SDE_ERROR("No BW, planes:%x dpms_mode:%d lpmode:%d\n",
          |                         ^~~~~~~~~
    
    Change-Id: Iab3b8dd3e0f8c9fe23638064a76e9c9b540a9b1c
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- techpack/display/msm/sde/sde_core_perf.c
+++ techpack/display/msm/sde/sde_core_perf.c
@@ -137,7 +137,7 @@ static void _sde_core_perf_calc_doze_suspend(struct drm_crtc *crtc,
 		}
 
 		if (!is_doze_suspend && conn && c_conn)
-			SDE_ERROR("No BW, planes:%x dpms_mode:%d lpmode:%d\n",
+			SDE_ERROR("No BW, planes:%x dpms_mode:%d lpmode:%llu\n",
 				state->plane_mask, c_conn->dpms_mode,
 				sde_connector_get_lp(conn));
 		if (conn && c_conn)
