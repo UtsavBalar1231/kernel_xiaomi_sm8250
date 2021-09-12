52ed5077809741d29cf8afdf7a4448ada5164f00
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-09-12 17:42:37 +0530

    cpufreq-hw: Fix format warning in cpufreq_hw_register_cooling_device
    In file included from ../include/linux/printk.h:7,
                     from ../include/linux/kernel.h:13,
                     from ../include/linux/clk.h:16,
                     from ../include/linux/cpufreq.h:15,
                     from ../drivers/cpufreq/qcom-cpufreq-hw.c:7:
    ../drivers/cpufreq/qcom-cpufreq-hw.c: In function 'cpufreq_hw_register_cooling_device':
    ../include/linux/kern_levels.h:5:25: warning: format '%d' expects argument of type 'int', but argument 3 has type 'long int' [-Wformat=]
        5 | #define KERN_SOH        "\001"          /* ASCII Start Of Header */
          |                         ^~~~~~
    ../include/linux/kern_levels.h:11:25: note: in expansion of macro 'KERN_SOH'
       11 | #define KERN_ERR        KERN_SOH "3"    /* error conditions */
          |                         ^~~~~~~~
    ../include/linux/printk.h:304:16: note: in expansion of macro 'KERN_ERR'
      304 |         printk(KERN_ERR pr_fmt(fmt), ##__VA_ARGS__)
          |                ^~~~~~~~
    ../drivers/cpufreq/qcom-cpufreq-hw.c:811:41: note: in expansion of macro 'pr_err'
      811 |                                         pr_err("Cooling register failed for %s, ret: %d\n",
          |                                         ^~~~~~
    
    Change-Id: I016ec3da314158040e95149c4c7082cfec2c2df2
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -810,7 +810,7 @@ static int cpufreq_hw_register_cooling_device(struct platform_device *pdev)
 						cpu_cdev,
 						&cpufreq_hw_cooling_ops);
 				if (IS_ERR(cpu_cdev->cdev)) {
-					pr_err("Cooling register failed for %s, ret: %d\n",
+					pr_err("Cooling register failed for %s, ret: %ld\n",
 						cdev_name,
 						PTR_ERR(cpu_cdev->cdev));
 					c->skip_data.final_index =
