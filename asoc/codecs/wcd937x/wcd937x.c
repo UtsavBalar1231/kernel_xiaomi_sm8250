ca52715670cecf4501031a1bdc109d13483225fc
Vatsal Bucha <vbucha@codeaurora.org>
2021-09-15 12:13:44 +0530

    ASoC: wcd937x: Update register value for new fab id for tanggu
    Update register value for new fab id for tanggu.
    
    Change-Id: I13594dbd311bbeab115753e0bec1a748158f8100
    Signed-off-by: Vatsal Bucha <vbucha@codeaurora.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- asoc/codecs/wcd937x/wcd937x.c
+++ asoc/codecs/wcd937x/wcd937x.c
@@ -141,7 +141,7 @@ static int wcd937x_init_reg(struct snd_soc_component *component)
 				0xFF, 0xFA);
 	snd_soc_component_update_bits(component, WCD937X_MICB3_TEST_CTL_1,
 				0xFF, 0xFA);
-	/* Set VBG Voltage to P0.5V for Tanggu second source */
+	/* Set Bandgap Fine Adjustment to +5mV for Tanggu SMIC part */
 	if (snd_soc_component_read32(component, WCD937X_DIGITAL_EFUSE_REG_16)
 	    == 0x01) {
 		snd_soc_component_update_bits(component,
@@ -152,6 +152,10 @@ static int wcd937x_init_reg(struct snd_soc_component *component)
 				WCD937X_HPH_NEW_INT_RDAC_HD2_CTL_L, 0x1F, 0x04);
 		snd_soc_component_update_bits(component,
 				WCD937X_HPH_NEW_INT_RDAC_HD2_CTL_R, 0x1F, 0x04);
+		snd_soc_component_update_bits(component,
+				WCD937X_BIAS_VBG_FINE_ADJ, 0xF0, 0xB0);
+		snd_soc_component_update_bits(component,
+				WCD937X_RX_BIAS_HPH_LOWPOWER, 0xF0, 0x90);
 	}
 	return 0;
 }
