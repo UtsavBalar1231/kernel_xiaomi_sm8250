ac3609f25568f002978802b75dd66306ea5409c9
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-09-19 16:51:31 +0530

    disp: msm: dp: Fix format warning in dp_display_process_mst_hpd_high
    In file included from ../include/drm/drm_mm.h:49,
                     from ../include/drm/drmP.h:73,
                     from ../techpack/display/msm/sde/sde_connector.h:10,
                     from ../techpack/display/msm/dp/dp_display.c:16:
    ../techpack/display/msm/dp/dp_display.c: In function 'dp_display_process_mst_hpd_high':
    ../techpack/display/msm/dp/dp_debug.h:19:35: warning: format '%d' expects argument of type 'int', but argument 4 has type 'long unsigned int' [-Wformat=]
       19 |                         DRM_DEBUG("[msm-dp-debug][%-4d]"fmt, current->pid,   \
          |                                   ^~~~~~~~~~~~~~~~~~~~~~
    ../include/drm/drm_print.h:352:30: note: in definition of macro 'DRM_DEBUG'
      352 |         drm_dbg(DRM_UT_CORE, fmt, ##__VA_ARGS__)
          |                              ^~~
    ../techpack/display/msm/dp/dp_display.c:33:32: note: in expansion of macro 'DP_DEBUG'
       33 | #define DP_MST_DEBUG(fmt, ...) DP_DEBUG(fmt, ##__VA_ARGS__)
          |                                ^~~~~~~~
    ../techpack/display/msm/dp/dp_display.c:825:25: note: in expansion of macro 'DP_MST_DEBUG'
      825 |                         DP_MST_DEBUG("MSTM_CTRL is not cleared, wait %dus\n",
          |                         ^~~~~~~~~~~~
    In file included from ../include/linux/kernel.h:13,
                     from ../include/linux/list.h:9,
                     from ../include/linux/module.h:9,
                     from ../techpack/display/msm/dp/dp_display.c:6:
    ../include/linux/kern_levels.h:5:25: warning: format '%d' expects argument of type 'int', but argument 4 has type 'long unsigned int' [-Wformat=]
        5 | #define KERN_SOH        "\001"          /* ASCII Start Of Header */
          |                         ^~~~~~
    ../include/linux/printk.h:137:24: note: in definition of macro 'no_printk'
      137 |                 printk(fmt, ##__VA_ARGS__);             \
          |                        ^~~
    ../include/linux/kern_levels.h:15:25: note: in expansion of macro 'KERN_SOH'
       15 | #define KERN_DEBUG      KERN_SOH "7"    /* debug-level messages */
          |                         ^~~~~~~~
    ../include/linux/printk.h:342:19: note: in expansion of macro 'KERN_DEBUG'
      342 |         no_printk(KERN_DEBUG pr_fmt(fmt), ##__VA_ARGS__)
          |                   ^~~~~~~~~~
    ../techpack/display/msm/dp/dp_debug.h:22:25: note: in expansion of macro 'pr_debug'
       22 |                         pr_debug("[drm:%s][msm-dp-debug][%-4d]"fmt, __func__,\
          |                         ^~~~~~~~
    ../techpack/display/msm/dp/dp_display.c:33:32: note: in expansion of macro 'DP_DEBUG'
       33 | #define DP_MST_DEBUG(fmt, ...) DP_DEBUG(fmt, ##__VA_ARGS__)
          |                                ^~~~~~~~
    ../techpack/display/msm/dp/dp_display.c:825:25: note: in expansion of macro 'DP_MST_DEBUG'
      825 |                         DP_MST_DEBUG("MSTM_CTRL is not cleared, wait %dus\n",
          |                         ^~~~~~~~~~~~
    
    Change-Id: Ifc25faac181fb439622f3b181307a912a3a44b3d
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- techpack/display/msm/dp/dp_display.c
+++ techpack/display/msm/dp/dp_display.c
@@ -822,7 +822,7 @@ static void dp_display_process_mst_hpd_high(struct dp_display_private *dp,
 
 		/* add extra delay if MST state is not cleared */
 		if (old_mstm_ctrl) {
-			DP_MST_DEBUG("MSTM_CTRL is not cleared, wait %dus\n",
+			DP_MST_DEBUG("MSTM_CTRL is not cleared, wait %lu us\n",
 					clear_mstm_ctrl_timeout_us);
 			usleep_range(clear_mstm_ctrl_timeout_us,
 				clear_mstm_ctrl_timeout_us + 1000);
