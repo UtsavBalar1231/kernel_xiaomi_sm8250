dc5800b0df3438b3eef7a08ce4e02f0125bf3c10
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-09-19 20:51:06 +0530

    msm: usb_bam: Fix misleading indentation warning in usb_bam_disconnect_ipa_prod
    ../drivers/platform/msm/usb_bam.c: In function 'usb_bam_disconnect_ipa_prod':
    ../drivers/platform/msm/usb_bam.c:1155:17: warning: this 'else' clause does not guard... [-Wmisleading-indentation]
     1155 |                 else
          |                 ^~~~
    ../drivers/platform/msm/usb_bam.c:1157:25: note: ...this statement, but the latter is misleadingly indented as if it were guarded by the 'else'
     1157 |                         spin_unlock(&ctx->usb_bam_lock);
          |                         ^~~~~~~~~~~
    
    Change-Id: If3445eb77aebefc3fe60e23846ca1677fe4a7363
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/platform/msm/usb_bam.c
+++ drivers/platform/msm/usb_bam.c
@@ -1155,7 +1155,7 @@ static int usb_bam_disconnect_ipa_prod(
 				__func__, pipe_connect->bam_type);
 		else
 			ctx->pipes_enabled_per_bam -= 1;
-			spin_unlock(&ctx->usb_bam_lock);
+		spin_unlock(&ctx->usb_bam_lock);
 	}
 
 	return 0;
