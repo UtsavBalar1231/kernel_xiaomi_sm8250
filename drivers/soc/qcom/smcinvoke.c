6beca0ad373fe3847000a4056ca52563b9551497
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-09-19 21:36:10 +0530

    qcom: smcinvoke: Fix format warning in process_tzcb_req
    In file included from ../include/linux/printk.h:7,
                     from ../include/linux/kernel.h:13,
                     from ../include/linux/list.h:9,
                     from ../include/linux/module.h:9,
                     from ../drivers/soc/qcom/smcinvoke.c:8:
    ../drivers/soc/qcom/smcinvoke.c: In function 'process_tzcb_req':
    ../include/linux/kern_levels.h:5:25: warning: format '%u' expects argument of type 'unsigned int', but argument 3 has type 'size_t' {aka 'long unsigned int'} [-Wformat=]
        5 | #define KERN_SOH        "\001"          /* ASCII Start Of Header */
          |                         ^~~~~~
    ../include/linux/kern_levels.h:11:25: note: in expansion of macro 'KERN_SOH'
       11 | #define KERN_ERR        KERN_SOH "3"    /* error conditions */
          |                         ^~~~~~~~
    ../include/linux/printk.h:304:16: note: in expansion of macro 'KERN_ERR'
      304 |         printk(KERN_ERR pr_fmt(fmt), ##__VA_ARGS__)
          |                ^~~~~~~~
    ../drivers/soc/qcom/smcinvoke.c:923:17: note: in expansion of macro 'pr_err'
      923 |                 pr_err("smaller buffer length : %u\n", buf_len);
          |                 ^~~~~~
    
    Change-Id: Ic0e05870d8d445051e1fa74832201314fca7d30b
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/soc/qcom/smcinvoke.c
+++ drivers/soc/qcom/smcinvoke.c
@@ -920,7 +920,7 @@ static void process_tzcb_req(void *buf, size_t buf_len, struct file **arr_filp)
 	struct smcinvoke_server_info *srvr_info = NULL;
 
 	if (buf_len < sizeof(struct smcinvoke_tzcb_req)) {
-		pr_err("smaller buffer length : %u\n", buf_len);
+		pr_err("smaller buffer length : %lu\n", buf_len);
 		return;
 	}
 
