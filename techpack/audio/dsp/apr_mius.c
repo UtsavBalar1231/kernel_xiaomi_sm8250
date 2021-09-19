bd26519dd8a7042de7239082a773f40d25b5670e
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-09-19 11:47:21 +0530

    techpack: block proximity sensor also for catogram/nekogramX
    Change-Id: I7cfa43e247a2f7da014bf0b76d56c27bbce58fc1
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- techpack/audio/dsp/apr_mius.c
+++ techpack/audio/dsp/apr_mius.c
@@ -394,7 +394,9 @@ int32_t mius_process_apr_payload(uint32_t *payload)
 	uint32_t payload_size = 0;
 	int32_t  ret = -1;
 
-	if (block_proximity("egram.messenger"))
+	if (block_proximity("egram.messenger") ||
+	    block_proximity("onlab.messenger") ||
+	    block_proximity("nekox.messenger"))
 		return ret;
 
 	//if (payload[0] == MIUS_ULTRASOUND_MODULE_TX) {
