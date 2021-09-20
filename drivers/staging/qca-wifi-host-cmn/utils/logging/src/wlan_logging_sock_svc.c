fba34def18345f6515d1c05d64347849c523f5a8
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-09-20 03:04:47 +0200

    qcacld-3.0: Fix misleading indentation in pktlog_send_per_pkt_stats_to_user
    ../drivers/staging/qcacld-3.0/../qca-wifi-host-cmn/utils/logging/src/wlan_logging_sock_svc.c: In function 'pktlog_send_per_pkt_stats_to_user':
    ../drivers/staging/qcacld-3.0/../qca-wifi-host-cmn/utils/logging/src/wlan_logging_sock_svc.c:609:9: error: this 'if' clause does not guard... [-Werror=misleading-indentation]
      609 |         if (free_old_skb)
          |         ^~
    In file included from ../include/linux/vmalloc.h:5,
                     from ../drivers/staging/qcacld-3.0/../qca-wifi-host-cmn/utils/logging/src/wlan_logging_sock_svc.c:25:
    ../include/linux/spinlock.h:358:1: note: ...this statement, but the latter is misleadingly indented as if it were guarded by the 'if'
      358 | do {                                                            \
          | ^~
    ../drivers/staging/qcacld-3.0/../qca-wifi-host-cmn/utils/logging/src/wlan_logging_sock_svc.c:612:17: note: in expansion of macro 'spin_lock_irqsave'
      612 |                 spin_lock_irqsave(&gwlan_logging.pkt_stats_lock, flags);
          |                 ^~~~~~~~~~~~~~~~~
      CC      drivers/staging/qcacld-3.0/../qca
    
    Change-Id: I49c0cbcd7ae35f2c23a828afa4b1f18591d11be0
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/staging/qca-wifi-host-cmn/utils/logging/src/wlan_logging_sock_svc.c
+++ drivers/staging/qca-wifi-host-cmn/utils/logging/src/wlan_logging_sock_svc.c
@@ -602,12 +602,12 @@ static int pktlog_send_per_pkt_stats_to_user(void)
 			ret = 0;
 		}
 err:
-	/*
-	 * Free old skb in case or error before assigning new skb
-	 * to the free list.
-	 */
-	if (free_old_skb)
-		dev_kfree_skb(pstats_msg->skb);
+		/*
+		 * Free old skb in case or error before assigning new skb
+		 * to the free list.
+		 */
+		if (free_old_skb)
+			dev_kfree_skb(pstats_msg->skb);
 
 		spin_lock_irqsave(&gwlan_logging.pkt_stats_lock, flags);
 		pstats_msg->skb = skb_new;
