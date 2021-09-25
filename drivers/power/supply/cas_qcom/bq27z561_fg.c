682cb38327410929b675a6aba6e6550900d14030
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-09-25 12:47:44 +0200

    cas: bq27z561_fg: Fix all the format specifier warnings
    In file included from ../include/linux/printk.h:7,
                     from ../include/linux/kernel.h:13,
                     from ../include/linux/list.h:9,
                     from ../include/linux/module.h:9,
                     from ../drivers/power/supply/cas_qcom/bq27z561_fg.c:18:
    ../drivers/power/supply/cas_qcom/bq27z561_fg.c: In function 'calc_delta_time':
    ../include/linux/kern_levels.h:5:25: warning: format '%ld' expects argument of type 'long int', but argument 3 has type 'ktime_t' {aka 'long long int'} [-Wformat=]
        5 | #define KERN_SOH        "\001"          /* ASCII Start Of Header */
          |                         ^~~~~~
    ../include/linux/kern_levels.h:14:25: note: in expansion of macro 'KERN_SOH'
       14 | #define KERN_INFO       KERN_SOH "6"    /* informational */
          |                         ^~~~~~~~
    ../include/linux/printk.h:311:16: note: in expansion of macro 'KERN_INFO'
      311 |         printk(KERN_INFO pr_fmt(fmt), ##__VA_ARGS__)
          |                ^~~~~~~~~
    ../drivers/power/supply/cas_qcom/bq27z561_fg.c:306:25: note: in expansion of macro 'pr_info'
      306 |                         pr_info(fmt, ##__VA_ARGS__);    \
          |                         ^~~~~~~
    ../drivers/power/supply/cas_qcom/bq27z561_fg.c:2291:9: note: in expansion of macro 'bq_dbg'
     2291 |         bq_dbg(PR_DEBUG,  "now:%ld, last:%ld, delta:%d\n", time_now, time_last, *delta_time);
          |         ^~~~~~
    ../include/linux/kern_levels.h:5:25: warning: format '%ld' expects argument of type 'long int', but argument 4 has type 'ktime_t' {aka 'long long int'} [-Wformat=]
        5 | #define KERN_SOH        "\001"          /* ASCII Start Of Header */
          |                         ^~~~~~
    ../include/linux/kern_levels.h:14:25: note: in expansion of macro 'KERN_SOH'
       14 | #define KERN_INFO       KERN_SOH "6"    /* informational */
          |                         ^~~~~~~~
    ../include/linux/printk.h:311:16: note: in expansion of macro 'KERN_INFO'
      311 |         printk(KERN_INFO pr_fmt(fmt), ##__VA_ARGS__)
          |                ^~~~~~~~~
    ../drivers/power/supply/cas_qcom/bq27z561_fg.c:306:25: note: in expansion of macro 'pr_info'
      306 |                         pr_info(fmt, ##__VA_ARGS__);    \
          |                         ^~~~~~~
    ../drivers/power/supply/cas_qcom/bq27z561_fg.c:2291:9: note: in expansion of macro 'bq_dbg'
     2291 |         bq_dbg(PR_DEBUG,  "now:%ld, last:%ld, delta:%d\n", time_now, time_last, *delta_time);
          |         ^~~~~~
    ../drivers/power/supply/cas_qcom/bq27z561_fg.c: In function 'bq_battery_soc_smooth_tracking':
    ../include/linux/kern_levels.h:5:25: warning: format '%ld' expects argument of type 'long int', but argument 4 has type 'ktime_t' {aka 'long long int'} [-Wformat=]
        5 | #define KERN_SOH        "\001"          /* ASCII Start Of Header */
          |                         ^~~~~~
    ../include/linux/kern_levels.h:14:25: note: in expansion of macro 'KERN_SOH'
       14 | #define KERN_INFO       KERN_SOH "6"    /* informational */
          |                         ^~~~~~~~
    ../include/linux/printk.h:311:16: note: in expansion of macro 'KERN_INFO'
      311 |         printk(KERN_INFO pr_fmt(fmt), ##__VA_ARGS__)
          |                ^~~~~~~~~
    ../drivers/power/supply/cas_qcom/bq27z561_fg.c:306:25: note: in expansion of macro 'pr_info'
      306 |                         pr_info(fmt, ##__VA_ARGS__);    \
          |                         ^~~~~~~
    ../drivers/power/supply/cas_qcom/bq27z561_fg.c:2378:25: note: in expansion of macro 'bq_dbg'
     2378 |                         bq_dbg(PR_DEBUG, "update last_statu:%d, last_change_time:%ld\n", status, last_change_time);
          |                         ^~~~~~
    
    Change-Id: If40ba249e88e8b5c00a97e5c3f8492a25d2fc97d
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/power/supply/cas_qcom/bq27z561_fg.c
+++ drivers/power/supply/cas_qcom/bq27z561_fg.c
@@ -2286,7 +2286,7 @@ static int calc_delta_time(ktime_t time_last, int *delta_time)
 	if (*delta_time < 0)
 		*delta_time = 0;
 
-	bq_dbg(PR_DEBUG,  "now:%ld, last:%ld, delta:%d\n", time_now, time_last, *delta_time);
+	bq_dbg(PR_DEBUG, "now:%lld, last:%lld, delta:%d\n", time_now, time_last, *delta_time);
 
 	return 0;
 }
@@ -2373,7 +2373,7 @@ static int bq_battery_soc_smooth_tracking(struct bq_fg_chip *bq,
 			if (last_status == POWER_SUPPLY_STATUS_CHARGING
 					&& status == POWER_SUPPLY_STATUS_DISCHARGING)
 				last_change_time = ktime_get();
-			bq_dbg(PR_DEBUG, "update last_statu:%d, last_change_time:%ld\n", status, last_change_time);
+			bq_dbg(PR_DEBUG, "update last_statu:%d, last_change_time:%lld\n", status, last_change_time);
 			last_status = status;
 		}
 	}
