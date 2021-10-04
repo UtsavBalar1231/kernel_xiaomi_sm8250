65a4d74065a014b663cd1e20f8c40428557dbda4
Weiyi Chen <quic_weiyic@quicinc.com>
2021-10-04 10:33:27 -0700

    BACKPORT: dfc: not caching grant for removed bearers
    If a grant indication for a bearer has "removed" flag set, caching
    the bearer could lead to data stall for the next data call because
    modem will not send grant for this bearer during powersave.
    
    Do not cache grant for the removed or disabled bearers. Also make sure
    a bearer is flow enabled when it is associated with a tx queue.
    
    Change-Id: I7eca597e3cc7d5a0bfe523201c454fb45e66a3a0
    Signed-off-by: Weiyi Chen <quic_weiyic@quicinc.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/soc/qcom/dfc_qmi.c
+++ drivers/soc/qcom/dfc_qmi.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <net/pkt_sched.h>
@@ -995,7 +996,9 @@ static int dfc_update_fc_map(struct net_device *dev, struct qos_info *qos,
 	u32 adjusted_grant;
 
 	itm = qmi_rmnet_get_bearer_map(qos, fc_info->bearer_id);
-	if (!itm)
+
+	/* cache the bearer assuming it is a new bearer */
+	if (unlikely(!itm && !is_query && fc_info->num_bytes))
 		itm = qmi_rmnet_get_bearer_noref(qos, fc_info->bearer_id);
 
 	if (itm) {
