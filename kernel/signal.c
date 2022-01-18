91bdbbc7f9faebe14fd1b5acb6e8317c5741b1cc
Linux Patches Robot <linux-patches-robot@chromeos-missing-patches.google.com.iam.gserviceaccount.com>
2022-01-18 01:06:19 +0000

    UPSTREAM: signal: In get_signal test for signal_group_exit every time through the loop
    Recently while investigating a problem with rr and signals I noticed
    that siglock is dropped in ptrace_signal and get_signal does not jump
    to relock.
    
    Looking farther to see if the problem is anywhere else I see that
    do_signal_stop also returns if signal_group_exit is true.  I believe
    that test can now never be true, but it is a bit hard to trace
    through and be certain.
    
    Testing signal_group_exit is not expensive, so move the test for
    signal_group_exit into the for loop inside of get_signal to ensure
    the test is never skipped improperly.
    
    This has been a potential problem since I added the test for
    signal_group_exit was added.
    
    Fixes: 35634ffa1751 ("signal: Always notice exiting tasks")
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Link: https://lkml.kernel.org/r/875yssekcd.fsf_-_@email.froward.int.ebiederm.org
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
    (cherry picked from commit e7f7c99ba911f56bc338845c1cd72954ba591707)
    
    BUG=None
    TEST=None
    
    Signed-off-by: Linux Patches Robot <linux-patches-robot@chromeos-missing-patches.google.com.iam.gserviceaccount.com>
    Change-Id: Iec0140650d1ba8f1ac37964cf774a324ab73e490
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3396556
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Tested-by: Guenter Roeck <groeck@chromium.org>
    Commit-Queue: Guenter Roeck <groeck@chromium.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -2514,22 +2514,22 @@ relock:
 		goto relock;
 	}
 
-	/* Has this task already been marked for death? */
-	if (signal_group_exit(signal)) {
-		ksig->info.si_signo = signr = SIGKILL;
-		sigdelset(&current->pending.signal, SIGKILL);
-		trace_signal_deliver(SIGKILL, SEND_SIG_NOINFO,
-				&sighand->action[SIGKILL - 1]);
-		recalc_sigpending();
-		current->jobctl &= ~JOBCTL_TRAP_FREEZE;
-		spin_unlock_irq(&sighand->siglock);
-		if (unlikely(cgroup_task_frozen(current)))
-			cgroup_leave_frozen(true);
-		goto fatal;
-	}
-
 	for (;;) {
 		struct k_sigaction *ka;
+
+		/* Has this task already been marked for death? */
+		if (signal_group_exit(signal)) {
+			ksig->info.si_signo = signr = SIGKILL;
+			sigdelset(&current->pending.signal, SIGKILL);
+			trace_signal_deliver(SIGKILL, SEND_SIG_NOINFO,
+					&sighand->action[SIGKILL - 1]);
+			recalc_sigpending();
+			current->jobctl &= ~JOBCTL_TRAP_FREEZE;
+			spin_unlock_irq(&sighand->siglock);
+			if (unlikely(cgroup_task_frozen(current)))
+				cgroup_leave_frozen(true);
+			goto fatal;
+		}
 
 		if (unlikely(current->jobctl & JOBCTL_STOP_PENDING) &&
 		    do_signal_stop(0))
