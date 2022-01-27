3814c07de852c88339090d5467f7fb12889710da
Cosmin Tanislav <demonsingur@gmail.com>
2022-01-27 02:08:11 +0200

    pstore: pmsg: pretty print android iovecs
    Change-Id: I3d4d88eda46fa70f653a8d47d020a709b187c97a
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- fs/pstore/pmsg.c
+++ fs/pstore/pmsg.c
@@ -15,26 +15,78 @@
 #include <linux/device.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
+#include <linux/uio.h>
 #include "internal.h"
 
 static DEFINE_MUTEX(pmsg_lock);
 
-static ssize_t write_pmsg(struct file *file, const char __user *buf,
-			  size_t count, loff_t *ppos)
+#define LOGGER_MAGIC		'l'
+#define LOG_ID_EVENTS		2
+
+enum android_log_priority {
+	ANDROID_LOG_UNKNOWN = 0,
+	ANDROID_LOG_DEFAULT,
+	ANDROID_LOG_VERBOSE,
+	ANDROID_LOG_DEBUG,
+	ANDROID_LOG_INFO,
+	ANDROID_LOG_WARN,
+	ANDROID_LOG_ERROR,
+	ANDROID_LOG_FATAL,
+	ANDROID_LOG_SILENT,
+};
+
+static const char log_characters[] = "XXVDIWEF";
+
+struct log_time {
+	uint32_t tv_sec;
+	uint32_t tv_nsec;
+} __attribute__((__packed__));
+
+struct android_pmsg_log_header {
+	uint8_t magic;
+	uint16_t len;
+	uint16_t uid;
+	uint16_t pid;
+} __attribute__((__packed__));
+
+struct android_log_header {
+	uint8_t id;
+	uint16_t tid;
+	struct log_time realtime;
+} __attribute__((__packed__));
+
+struct android_pmsg_prio_header {
+	uint8_t prio;
+} __attribute__((__packed__));
+
+struct android_pmsg_tag_header {
+	uint32_t tag;
+} __attribute__((__packed__));
+
+static ssize_t pmsg_write(char *buf, size_t count)
 {
 	struct pstore_record record;
 	int ret;
 
-	if (!count)
-		return 0;
+	pstore_record_init(&record, psinfo);
+	record.type = PSTORE_TYPE_PMSG;
+	record.size = count;
+	record.buf = buf;
+
+	mutex_lock(&pmsg_lock);
+	ret = psinfo->write(&record);
+	mutex_unlock(&pmsg_lock);
+	return ret ? ret : count;
+}
+
+static ssize_t pmsg_write_user(const char __user *buf, size_t count)
+{
+	struct pstore_record record;
+	int ret;
 
 	pstore_record_init(&record, psinfo);
 	record.type = PSTORE_TYPE_PMSG;
 	record.size = count;
-
-	/* check outside lock, page in any data. write_user also checks */
-	if (!access_ok(VERIFY_READ, buf, count))
-		return -EFAULT;
 
 	mutex_lock(&pmsg_lock);
 	ret = psinfo->write_user(&record, buf);
@@ -42,10 +94,154 @@ static ssize_t write_pmsg(struct file *file, const char __user *buf,
 	return ret ? ret : count;
 }
 
+#define pmsg_write_arr(arr) pmsg_write(arr, sizeof(arr) - 1)
+
+static void pmsg_write_iovec_str(const struct iovec *iov)
+{
+	void __user *buf = iov->iov_base;
+	size_t len = iov->iov_len - 1;
+
+	if (len <= 0)
+		return;
+
+	pmsg_write_user(buf, len);
+}
+
+static bool pmsg_is_pmsg_header(unsigned long part, const struct iovec *iov)
+{
+	struct android_pmsg_log_header pmsg_header;
+	void __user *buf = iov->iov_base;
+	size_t len = iov->iov_len;
+
+	int ret;
+
+	if (part != 0)
+		return false;
+
+	if (len != sizeof(pmsg_header))
+		return false;
+
+	ret = __copy_from_user(&pmsg_header, buf, len);
+	if (ret)
+		return false;
+
+	if (pmsg_header.magic != LOGGER_MAGIC)
+		return false;
+
+	return true;
+}
+
+static bool pmsg_is_header(unsigned long part, const struct iovec *iov)
+{
+	struct android_log_header header;
+	void __user *buf = iov->iov_base;
+	size_t len = iov->iov_len;
+	int ret;
+
+	if (part != 1)
+		return false;
+
+	if (len != sizeof(struct android_log_header))
+		return false;
+
+	ret = __copy_from_user(&header, buf, len);
+	if (ret)
+		return false;
+
+	if (header.id == LOG_ID_EVENTS)
+		return false;
+
+	return true;
+}
+
+static bool pmsg_is_prio(unsigned long part, const struct iovec *iov,
+			 char *priority)
+{
+	struct android_pmsg_prio_header prio_header;
+	void __user *buf = iov->iov_base;
+	size_t len = iov->iov_len;
+	int ret;
+
+	if (part != 2)
+		return false;
+
+	if (len != sizeof(struct android_pmsg_prio_header))
+		return false;
+
+	ret = __copy_from_user(&prio_header, buf, len);
+	if (ret)
+		return false;
+
+	if (prio_header.prio >= ANDROID_LOG_SILENT)
+		prio_header.prio = ANDROID_LOG_FATAL;
+
+	priority[0] = log_characters[prio_header.prio];
+
+	return true;
+}
+
+static bool pmsg_is_tag(unsigned long part, const struct iovec *iov)
+{
+	size_t len = iov->iov_len;
+
+	if (part != 2)
+		return false;
+
+	if (len != sizeof(struct android_pmsg_tag_header))
+		return false;
+
+	return true;
+}
+
+static bool pmsg_is_name_tag(unsigned long part)
+{
+	return part == 3;
+}
+
+static bool pmsg_is_message(unsigned long part)
+{
+	return part == 4;
+}
+
+char name_tag_end[] = ": ";
+char message_end[] = "\n";
+
+static ssize_t pmsg_write_iter(struct kiocb *iocb, struct iov_iter *from)
+{
+	size_t count = iov_iter_count(from);
+	unsigned long i;
+	char priority[] = "# ";
+
+	if (!iter_is_iovec(from))
+		return -EINVAL;
+
+	for (i = 0; i < from->nr_segs; i++) {
+		const struct iovec *iov = &from->iov[i];
+
+		if (pmsg_is_pmsg_header(i, iov) ||
+		    pmsg_is_header(i, iov) ||
+		    pmsg_is_tag(i, iov)) {
+			/* continue */
+		} else if (pmsg_is_prio(i, iov, priority)) {
+			pmsg_write_arr(priority);
+		} else if (pmsg_is_name_tag(i)) {
+			pmsg_write_iovec_str(iov);
+			pmsg_write_arr(name_tag_end);
+		} else if (pmsg_is_message(i)) {
+			pmsg_write_iovec_str(iov);
+			pmsg_write_arr(message_end);
+		} else {
+			break;
+		}
+	}
+
+	return count;
+}
+
 static const struct file_operations pmsg_fops = {
 	.owner		= THIS_MODULE,
 	.llseek		= noop_llseek,
-	.write		= write_pmsg,
+	.write_iter = pmsg_write_iter,
 };
 
 static struct class *pmsg_class;
