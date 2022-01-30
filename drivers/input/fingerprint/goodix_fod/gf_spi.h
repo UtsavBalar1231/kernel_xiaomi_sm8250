a42569e8110a74691bf236c57f6a9d20c438a3b4
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-01-30 13:17:47 +0530

    input: fingerprint: goodix_fod: Enable 3V0 regulator on psyche (L3A)
    Change-Id: I468a33637b9609da464a63af43d06cabf6fac214
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/input/fingerprint/goodix_fod/gf_spi.h
+++ drivers/input/fingerprint/goodix_fod/gf_spi.h
@@ -105,7 +105,13 @@ struct gf_ioc_chip_info {
 #define USE_PLATFORM_BUS		1
 /*#define  USE_SPI_BUS  1*/
 			 /*#define GF_FASYNC   1*//*If support fasync mechanism. */
+
+#if IS_ENABLED(CONFIG_BOARD_PSYCHE)
+#undef GF_PW_CTL
+#else
 #define GF_PW_CTL 1
+#endif
+
 #define GF_NETLINK_ENABLE 1
 #define GF_NET_EVENT_IRQ 1
 #define GF_NET_EVENT_FB_BLACK 2
