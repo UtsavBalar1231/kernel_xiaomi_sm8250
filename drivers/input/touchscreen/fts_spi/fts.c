496d73eebc1366d2286380b062dc92a3192bb86c
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-02-05 18:04:52 +0100

    drivers: input: touchscreen: fts_spi: Implement double_tap node
    Change-Id: I2256e94e88e6f493b1af658f3a2149b22249e172
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/input/touchscreen/fts_spi/fts.c
+++ drivers/input/touchscreen/fts_spi/fts.c
@@ -87,6 +87,12 @@
 #include "../xiaomi/xiaomi_touch.h"
 #endif
 
+#ifdef GESTURE_MODE
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+#include <linux/input/tp_common.h>
+#endif
+#endif
+
 /**
  * Event handler installer helpers
  */
@@ -515,6 +521,33 @@ static ssize_t fts_feature_enable_show(struct device *dev,
 	return count;
 }
 #else
+
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+static ssize_t double_tap_show(struct kobject *kobj,
+				struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%d\n", fts_info->gesture_enabled);
+}
+
+static ssize_t double_tap_store(struct kobject *kobj,
+	struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	int rc, val;
+
+	rc = kstrtoint(buf, 10, &val);
+	if (rc)
+		return -EINVAL;
+
+	fts_info->gesture_enabled = !!val;
+	schedule_work(&fts_info->switch_mode_work);
+	return count;
+}
+
+static struct tp_common_ops double_tap_ops = {
+	.show = double_tap_show,
+	.store = double_tap_store
+};
+#endif
 
 #ifdef GRIP_MODE
 /**
@@ -8306,6 +8339,9 @@ static int fts_probe(struct spi_device *client)
 	int retval;
 	int skip_5_1 = 0;
 	u16 bus_type;
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+	int ret;
+#endif
 #if defined(CONFIG_DRM_PANEL) && defined(CONFIG_OF)
 	error = fts_ts_check_panel(dp);
 	if (error < 0)
@@ -8536,6 +8572,12 @@ static int fts_probe(struct spi_device *client)
 	mutex_init(&(info->input_report_mutex));
 #ifdef GESTURE_MODE
 	mutex_init(&gestureMask_mutex);
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+	ret = tp_common_set_double_tap_ops(&double_tap_ops);
+	if (ret < 0)
+		MI_TOUCH_LOGE(1, "%s %s: Failed to create double_tap node err=%d\n",
+			tag, __func__, ret);
+#endif
 #endif
 
 	spin_lock_init(&fts_int);
