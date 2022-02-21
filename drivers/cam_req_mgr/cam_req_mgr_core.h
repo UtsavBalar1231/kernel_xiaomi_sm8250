7811469f79ccc3124ef9eaf79d1edeb32c6a87ec
Yash Upadhyay <quic_yupadhya@quicinc.com>
2022-02-21 10:07:23 +0530

    msm: camera: reqmgr: Validate the link handle
    Instead of correct link handle, if some other handle like
    dev handle is passed then it may access some other data space.
    To avoid such scenario, need to check whether link handle
    passed by ioctl is same as retrieved link handle.
    
    CRs-Fixed: 3120454
    Change-Id: Idff2e3c25b60563788ffb426c7cabc367c3c97f8
    Signed-off-by: Yash Upadhyay <quic_yupadhya@quicinc.com>

--- drivers/cam_req_mgr/cam_req_mgr_core.h
+++ drivers/cam_req_mgr/cam_req_mgr_core.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2016-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 #ifndef _CAM_REQ_MGR_CORE_H_
 #define _CAM_REQ_MGR_CORE_H_
@@ -17,6 +18,7 @@
 #define CAM_REQ_MGR_WATCHDOG_TIMEOUT_MAX   50000
 #define CAM_REQ_MGR_SCHED_REQ_TIMEOUT      1000
 #define CAM_REQ_MGR_SIMULATE_SCHED_REQ     30
+#define CAM_REQ_MGR_DEFAULT_HDL_VAL        0
 
 #define FORCE_DISABLE_RECOVERY  2
 #define FORCE_ENABLE_RECOVERY   1
