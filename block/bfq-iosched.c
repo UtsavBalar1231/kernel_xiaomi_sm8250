fb6d011c2647572ec7ce9cab09a640ae22dce602
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-04-20 17:48:35 +0530

    Revert "block: use current active bfqq to update statistics"
    Conflits with UPSTREAM patch
    This reverts commit 1852d0be8d78f77ee73527ccff73ace073267851.
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -4068,6 +4068,7 @@ exit:
 #if defined(CONFIG_BFQ_GROUP_IOSCHED) && defined(CONFIG_DEBUG_BLK_CGROUP)
 static void bfq_update_dispatch_stats(struct request_queue *q,
 				      struct request *rq,
+				      struct bfq_queue *in_serv_queue,
 				      bool idle_timer_disabled)
 {
 	struct bfq_queue *bfqq = rq ? RQ_BFQQ(rq) : NULL;
@@ -4089,15 +4090,17 @@ static void bfq_update_dispatch_stats(struct request_queue *q,
 	 * bfqq_group(bfqq) exists as well.
 	 */
 	spin_lock_irq(q->queue_lock);
-	if (bfqq && idle_timer_disabled)
+	if (idle_timer_disabled)
 		/*
-		 * It could be possible that current active
-		 * queue and group might got updated along with
-		 * request via. __bfq_dispatch_request.
-		 * So, always use current active request to
-		 * derive its associated bfq queue and group.
+		 * Since the idle timer has been disabled,
+		 * in_serv_queue contained some request when
+		 * __bfq_dispatch_request was invoked above, which
+		 * implies that rq was picked exactly from
+		 * in_serv_queue. Thus in_serv_queue == bfqq, and is
+		 * therefore guaranteed to exist because of the above
+		 * arguments.
 		 */
-		bfqg_stats_update_idle_time(bfqq_group(bfqq));
+		bfqg_stats_update_idle_time(bfqq_group(in_serv_queue));
 	if (bfqq) {
 		struct bfq_group *bfqg = bfqq_group(bfqq);
 
@@ -4110,6 +4113,7 @@ static void bfq_update_dispatch_stats(struct request_queue *q,
 #else
 static inline void bfq_update_dispatch_stats(struct request_queue *q,
 					     struct request *rq,
+					     struct bfq_queue *in_serv_queue,
 					     bool idle_timer_disabled) {}
 #endif
 
@@ -4132,7 +4136,7 @@ static struct request *bfq_dispatch_request(struct blk_mq_hw_ctx *hctx)
 
 	spin_unlock_irq(&bfqd->lock);
 
-	bfq_update_dispatch_stats(hctx->queue, rq,
+	bfq_update_dispatch_stats(hctx->queue, rq, in_serv_queue,
 				  idle_timer_disabled);
 
 	return rq;
