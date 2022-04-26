026b67c403cbe44ee1e7bf45a4e90ee9dddaab6e
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-04-26 21:11:01 +0530

    Revert "mm: Fix sleeping while atomic during speculative page fault"
    This reverts commit 8b8dd3be3358e4f3db8de766580b04f1eec94c7f.
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- mm/memory.c
+++ mm/memory.c
@@ -3735,7 +3735,7 @@ static vm_fault_t pte_alloc_one_map(struct vm_fault *vmf)
 {
 	struct vm_area_struct *vma = vmf->vma;
 
-	if (!pmd_none(*vmf->pmd) || (vmf->flags & FAULT_FLAG_SPECULATIVE))
+	if (!pmd_none(*vmf->pmd))
 		goto map_pte;
 	if (vmf->prealloc_pte) {
 		vmf->ptl = pmd_lock(vma->vm_mm, vmf->pmd);
