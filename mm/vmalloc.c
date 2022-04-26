dd134ab82a5f17643297791cf68cfbc2a0db9f62
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-04-26 21:13:12 +0530

    Revert "mm/vmalloc.c: avoid bogus -Wmaybe-uninitialized warning"
    This reverts commit aaac8edf58ba1f40331f6cebf74c52f167d46172.
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -921,7 +921,7 @@ adjust_va_to_fit_type(struct vmap_area *va,
 	unsigned long nva_start_addr, unsigned long size,
 	enum fit_type type)
 {
-	struct vmap_area *lva = NULL;
+	struct vmap_area *lva;
 
 	if (type == FL_FIT_TYPE) {
 		/*
@@ -980,7 +980,7 @@ adjust_va_to_fit_type(struct vmap_area *va,
 	if (type != FL_FIT_TYPE) {
 		augment_tree_propagate_from(va);
 
-		if (lva)	/* type == NE_FIT_TYPE */
+		if (type == NE_FIT_TYPE)
 			insert_vmap_area_augment(lva, &va->rb_node,
 				&free_vmap_area_root, &free_vmap_area_list);
 	}
