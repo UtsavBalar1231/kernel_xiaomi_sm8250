64cfb579ec5fcd080382e99c14dc6aaa1c150fef
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-04-28 19:10:12 +0530

    Revert "mm: add speculative page fault vmstats"
    This reverts commit fa7f0de09638c2e2d4a0b30f134a8f597f85770c.
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1296,10 +1296,7 @@ const char * const vmstat_text[] = {
 	"swap_ra",
 	"swap_ra_hit",
 #endif
-#ifdef CONFIG_SPECULATIVE_PAGE_FAULT
-	"speculative_pgfault"
-#endif
-#endif /* CONFIG_VM_EVENT_COUNTERS */
+#endif /* CONFIG_VM_EVENTS_COUNTERS */
 };
 #endif /* CONFIG_PROC_FS || CONFIG_SYSFS || CONFIG_NUMA */
 
