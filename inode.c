bf7b4057f4fd3f93726211a6aa112ac792519f09
Christoph Hellwig <hch@lst.de>
2022-05-11 15:32:40 +0900

    exfat: mm: require ->set_page_dirty to be explicitly wired up
    Remove the CONFIG_BLOCK default to __set_page_dirty_buffers and just wire
    that method up for the missing instances.
    
    [hch@lst.de: ecryptfs: add a ->set_page_dirty cludge]
      Link: https://lkml.kernel.org/r/20210624125250.536369-1-hch@lst.de
    
    Link: https://lkml.kernel.org/r/20210614061512.3966143-4-hch@lst.de
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Tyler Hicks <code@tyhicks.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>

--- inode.c
+++ inode.c
@@ -514,6 +514,9 @@ int exfat_block_truncate_page(struct inode *inode, loff_t from)
 }
 
 static const struct address_space_operations exfat_aops = {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 14, 0)
+	.set_page_dirty	= __set_page_dirty_buffers,
+#endif
 	.readpage	= exfat_readpage,
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 8, 0)
 	.readahead	= exfat_readahead,
