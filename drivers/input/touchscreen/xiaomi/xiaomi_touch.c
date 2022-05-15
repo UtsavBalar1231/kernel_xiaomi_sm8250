3cec2d00ce5bdaa603cd7cbd992c8a9df2a6bb30
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-05-15 19:02:44 +0530

    touchscreen: xiaomi_touch: Fix strict-prototypes warnings in function declaration
    ../drivers/input/touchscreen/xiaomi/xiaomi_touch.c:141:37: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
    struct class *get_xiaomi_touch_class()
                                        ^
                                         void
    ../drivers/input/touchscreen/xiaomi/xiaomi_touch.c:146:36: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
    struct device *get_xiaomi_touch_dev()
                                       ^
                                        void
    2 errors generated.

--- drivers/input/touchscreen/xiaomi/xiaomi_touch.c
+++ drivers/input/touchscreen/xiaomi/xiaomi_touch.c
@@ -138,12 +138,12 @@ struct xiaomi_touch *xiaomi_touch_dev_get(int minor)
 		return NULL;
 }
 
-struct class *get_xiaomi_touch_class()
+struct class *get_xiaomi_touch_class(void)
 {
 	return xiaomi_touch_dev.class;
 }
 
-struct device *get_xiaomi_touch_dev()
+struct device *get_xiaomi_touch_dev(void)
 {
 	return xiaomi_touch_dev.dev;
 }
