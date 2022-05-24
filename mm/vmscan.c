17fbd4d621a74337c98ce4b4f9f0d576d1b699a6
Juhyung Park <qkrwngud825@gmail.com>
2022-05-24 22:22:56 +0900

    mm: multi-gen LRU: set min_ttl to 5000ms by default
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3904,7 +3904,7 @@ static bool age_lruvec(struct lruvec *lruvec, struct scan_control *sc,
 }
 
 /* to protect the working set of the last N jiffies */
-static unsigned long lru_gen_min_ttl __read_mostly;
+static unsigned long lru_gen_min_ttl __read_mostly = 5 * HZ; // 5000ms
 static int lru_gen_min_ttl_unsatisfied;
 
 static void lru_gen_age_node(struct pglist_data *pgdat, struct scan_control *sc)
