8f17127d0248e4300c7e3e1c5f285111d9eb4b88
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-05-25 09:52:15 +0530

    Revert "BACKPORT: FROMLIST: mm: multigenerational lru: eviction"
    This reverts commit 0dbe4aa7b4a10da34fba13532f484928762c6464.

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -757,8 +757,6 @@ struct zonelist {
 extern struct page *mem_map;
 #endif
 
-struct mm_walk_args;
-
 /*
  * On NUMA machines, each NUMA node would have a pg_data_t to describe
  * it's memory layout. On UMA machines there is a single pglist_data which
@@ -855,9 +853,6 @@ typedef struct pglist_data {
 
 	unsigned long		flags;
 
-#ifdef CONFIG_LRU_GEN
-	struct mm_walk_args	*mm_walk_args;
-#endif
 	ZONE_PADDING(_pad2_)
 
 	/* Per-node vmstats */
