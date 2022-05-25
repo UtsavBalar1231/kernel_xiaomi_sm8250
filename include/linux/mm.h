4eaac3989f3d65907eba0f5ed380e65c0d39d310
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-05-25 09:52:19 +0530

    Revert "BACKPORT: FROMLIST: include/linux/mm.h: do not warn in page_memcg_rcu() if !CONFIG_MEMCG"
    This reverts commit 69e15beeed78a06893c0245d8f0ffb9397dadcbb.

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1210,6 +1210,7 @@ static inline struct mem_cgroup *page_memcg(struct page *page)
 }
 static inline struct mem_cgroup *page_memcg_rcu(struct page *page)
 {
+	WARN_ON_ONCE(!rcu_read_lock_held());
 	return NULL;
 }
 #endif
