5662fcca4562cdd3d282206ec2a1aed19602ab9d
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-06-06 10:26:09 +0530

    lz4: Compile LZ4_decompress_safe_partial() when EROFS_FS is enabled
    Fixes following error:
    ../fs/erofs/decompressor.c:226:9: error: implicit declaration of function 'LZ4_decompress_safe_partial' [-Werror,-Wimplicit-function-declaration]
                    ret = LZ4_decompress_safe_partial(src + inputmargin, out,
                          ^
    
    Change-Id: I4eb111456d755154492dd8918866a53c38682ebd
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- include/linux/lz4.h
+++ include/linux/lz4.h
@@ -296,7 +296,7 @@ int LZ4_decompress_fast(const char *source, char *dest, int originalSize);
 int LZ4_decompress_safe(const char *source, char *dest, int compressedSize,
 	int maxDecompressedSize);
 
-#if 0
+#if IS_ENABLED(CONFIG_EROFS_FS)
 /**
  * LZ4_decompress_safe_partial() - Decompress a block of size 'compressedSize'
  *	at position 'source' into buffer 'dest'
@@ -322,7 +322,7 @@ int LZ4_decompress_safe(const char *source, char *dest, int compressedSize,
  *	or a negative result in case of error
  *
  */
-static int LZ4_decompress_safe_partial(const char *source, char *dest,
+int LZ4_decompress_safe_partial(const char *source, char *dest,
 	int compressedSize, int targetOutputSize, int maxDecompressedSize);
 #endif
 
--- lib/lz4/lz4_decompress.c
+++ lib/lz4/lz4_decompress.c
@@ -463,8 +463,8 @@ int LZ4_decompress_safe(const char *source, char *dest,
 				      noDict, (BYTE *)dest, NULL, 0);
 }
 
-#if 0
-static int LZ4_decompress_safe_partial(const char *src, char *dst,
+#if IS_ENABLED(CONFIG_EROFS_FS)
+int LZ4_decompress_safe_partial(const char *src, char *dst,
 	int compressedSize, int targetOutputSize, int dstCapacity)
 {
 	dstCapacity = min(targetOutputSize, dstCapacity);
