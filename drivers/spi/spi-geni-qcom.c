69ef63f61cb0fa1f4bec6ca13760fe3522c5c32c
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-07-02 11:00:29 +0000

    spi: qcom: Make it probe asynchronously
    Change-Id: Ie2234d23a656f687606e5e1965369826cfeec2ed
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/spi/spi-geni-qcom.c
+++ drivers/spi/spi-geni-qcom.c
@@ -1962,6 +1962,7 @@ static struct platform_driver spi_geni_driver = {
 		.name = "spi_geni",
 		.pm = &spi_geni_pm_ops,
 		.of_match_table = spi_geni_dt_match,
+		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
 	},
 };
 module_platform_driver(spi_geni_driver);
