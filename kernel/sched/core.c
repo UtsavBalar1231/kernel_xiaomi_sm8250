f12f206b5339ad1294642e89195fe08918fb2459
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-08-11 14:39:46 +0530

    kernel: Import xiaomi user experience optimization modifications
    From: micode/Xiaomi_Kernel_Opensource/
    branch: munch-s-oss
    
    Change-Id: I3028b7cc6a64f6b3c4e9c943dd6081cfc51f5374
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -739,6 +739,14 @@ static void set_load_weight(struct task_struct *p, bool update_load)
 	}
 }
 
+#if IS_ENABLED(CONFIG_PACKAGE_RUNTIME_INFO)
+void __weak migt_monitor_hook(int enqueue, int cpu,
+		struct task_struct *p, u64 walltime)
+{
+	/*do nothing*/
+}
+#endif
+
 #ifdef CONFIG_UCLAMP_TASK
 /*
  * Serializes updates of utilization clamp values
@@ -1335,6 +1343,9 @@ static inline void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 	uclamp_rq_inc(rq, p);
 	p->sched_class->enqueue_task(rq, p, flags);
 	walt_update_last_enqueue(p);
+#if IS_ENABLED(CONFIG_PACKAGE_RUNTIME_INFO)
+	migt_monitor_hook(1, rq->cpu, p, sched_ktime_clock());
+#endif
 	trace_sched_enq_deq_task(p, 1, cpumask_bits(&p->cpus_allowed)[0]);
 }
 
@@ -1353,6 +1364,9 @@ static inline void dequeue_task(struct rq *rq, struct task_struct *p, int flags)
 #ifdef CONFIG_SCHED_WALT
 	if (p == rq->ed_task)
 		early_detection_notify(rq, sched_ktime_clock());
+#endif
+#if IS_ENABLED(CONFIG_PACKAGE_RUNTIME_INFO)
+	migt_monitor_hook(0, rq->cpu, p, sched_ktime_clock());
 #endif
 	trace_sched_enq_deq_task(p, 0, cpumask_bits(&p->cpus_allowed)[0]);
 }
@@ -1630,6 +1644,10 @@ void set_cpus_allowed_common(struct task_struct *p, const struct cpumask *new_ma
 {
 	cpumask_copy(&p->cpus_allowed, new_mask);
 	p->nr_cpus_allowed = cpumask_weight(new_mask);
+#if IS_ENABLED(CONFIG_PACKAGE_RUNTIME_INFO)
+	p->pkg.migt.flag &= ~MINOR_TASK;
+	cpumask_copy(&p->pkg.migt.cpus_allowed, new_mask);
+#endif
 }
 
 void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
@@ -2187,6 +2205,27 @@ int select_task_rq(struct task_struct *p, int cpu, int sd_flags, int wake_flags,
 		   int sibling_count_hint)
 {
 	bool allow_isolated = (p->flags & PF_KTHREAD);
+#if IS_ENABLED(CONFIG_PACKAGE_RUNTIME_INFO)
+	bool minor_wtask = minor_window_task(p);
+	cpumask_t minor_window_cpumask;
+
+	if (minor_wtask && !(p->pkg.migt.flag & MINOR_TASK)) {
+		p->pkg.migt.flag |= MINOR_TASK;
+		cpumask_copy(&p->pkg.migt.cpus_allowed, &p->cpus_allowed);
+		if (get_minor_window_cpumask(p, &minor_window_cpumask)) {
+			cpumask_copy(&p->cpus_allowed, &minor_window_cpumask);
+			p->nr_cpus_allowed = cpumask_weight(&minor_window_cpumask);
+		}
+	}
+
+	if (!minor_wtask && (p->pkg.migt.flag & MINOR_TASK)) {
+		p->pkg.migt.flag &= ~MINOR_TASK;
+		if (get_minor_window_cpumask(p, &minor_window_cpumask)) {
+			cpumask_copy(&p->cpus_allowed, &p->pkg.migt.cpus_allowed);
+			p->nr_cpus_allowed = cpumask_weight(&p->cpus_allowed);
+		}
+	}
+#endif
 	lockdep_assert_held(&p->pi_lock);
 
 	if (p->nr_cpus_allowed > 1)
@@ -2876,6 +2915,9 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 #endif
 	INIT_LIST_HEAD(&p->se.group_node);
 
+#if IS_ENABLED(CONFIG_PACKAGE_RUNTIME_INFO)
+	init_task_runtime_info(p);
+#endif
 #ifdef CONFIG_FAIR_GROUP_SCHED
 	p->se.cfs_rq			= NULL;
 #endif
@@ -5667,6 +5709,24 @@ long sched_setaffinity(pid_t pid, const struct cpumask *in_mask)
 	if (retval)
 		goto out_free_new_mask;
 
+#if IS_ENABLED(CONFIG_PACKAGE_RUNTIME_INFO)
+	if (minor_window_task(p)) {
+		retval = -EPERM;
+		cpuset_cpus_allowed(p, cpus_allowed);
+		cpumask_and(new_mask, in_mask, cpus_allowed);
+		cpumask_andnot(&allowed_mask, new_mask, cpu_isolated_mask);
+		dest_cpu = cpumask_any_and(cpu_active_mask, &allowed_mask);
+		if (dest_cpu < nr_cpu_ids) {
+			 cpuset_cpus_allowed(p, cpus_allowed);
+			  if (!cpumask_subset(new_mask, cpus_allowed))
+				  cpumask_copy(new_mask, cpus_allowed);
+			  cpumask_copy(&p->pkg.migt.cpus_allowed, new_mask);
+		}
+		p->pkg.migt.flag |= MINOR_TASK;
+		cpumask_copy(&p->pkg.migt.cpus_allowed, new_mask);
+		goto out_free_new_mask;
+	}
+#endif
 
 	cpuset_cpus_allowed(p, cpus_allowed);
 	cpumask_and(new_mask, in_mask, cpus_allowed);
@@ -8734,7 +8794,7 @@ inline void restore_inherit_top_app(struct task_struct *p)
 	if (p && is_inherit_top_app(p)) {
 		p->inherit_top_app = 0;
 #if IS_ENABLED(CONFIG_PERF_HUMANTASK)
-		p->human_task  = 0 ;
+		p->human_task  = 0;
 #endif
 	}
 }
