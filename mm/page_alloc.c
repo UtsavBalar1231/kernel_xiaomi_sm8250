e7bde042b877dbc2c832abd65684145ae34ccfa8
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-08-13 07:52:21 +0000

    mm: page_alloc: Hardcode min_free_kbytes to 32768 kb
    Change-Id: I08355acd995e956c63cc0d3f1587604e39f91269
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -322,7 +322,7 @@ compound_page_dtor * const compound_page_dtors[] = {
  * allocations below this point, only high priority ones. Automatically
  * tuned according to the amount of memory in the system.
  */
-int min_free_kbytes = 1024;
+int min_free_kbytes = 32768;
 int user_min_free_kbytes = -1;
 #ifdef CONFIG_DISCONTIGMEM
 /*
@@ -7971,8 +7971,8 @@ int __meminit init_per_zone_wmark_min(void)
 
 	if (new_min_free_kbytes > user_min_free_kbytes) {
 		min_free_kbytes = new_min_free_kbytes;
-		if (min_free_kbytes < 128)
-			min_free_kbytes = 128;
+		if (min_free_kbytes < 32768)
+			min_free_kbytes = 32768;
 		if (min_free_kbytes > 65536)
 			min_free_kbytes = 65536;
 	} else {
