5ed7ede27772e9cc3f387c39cc76699c5dac1d6a
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-08-19 15:23:11 +0000

    net: Guard BPF_GET_COMM_HASH uapi for MIUI builds
    Commit: 55fc8bd1edbc0de0ec7c18118ae2c0cb5e92f0b7
    BPF_GET_COMM_HASH seems to be breaking android13 bpf loader.
    Therefore, disable it for non MIUI builds to allow bpf loader
    to boot on android13 ROMs.
    
    Change-Id: I9f5c74760a75221d5ee8c8ead6c6f84a2dd5f812
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- include/net/sock.h
+++ include/net/sock.h
@@ -477,7 +477,9 @@ struct sock {
 #else
 	/* sk_peer_lock is in the ANDROID_KABI_RESERVE(1) field below */
 #endif
+#if IS_ENABLED(CONFIG_MIHW)
 	pid_t			pid_num;
+#endif
 	struct pid		*sk_peer_pid;
 	const struct cred	*sk_peer_cred;
 
